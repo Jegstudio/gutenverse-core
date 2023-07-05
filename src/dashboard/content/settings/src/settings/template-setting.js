f2d64b79b2366b71204a73fec054533aacb8483c
jegbagus <agungbayuiswara@gmail.com>
2023-07-05 17:11:43 +0800

    Option to use homepage setting
    option to use homepage setting instead of front-page template

--- src/dashboard/content/settings/src/settings/template-setting.js
+++ src/dashboard/content/settings/src/settings/template-setting.js
@@ -13,6 +13,15 @@ const TemplateSetting = ({ settingValues, updateSettingValues, saving, saveData 
     return <div>
         <div className="template-tab-body">
             <ControlCheckbox
+                id={'use_setting_homepage'}
+                title={__('Use Reading Homepage Setting', 'gutenverse')}
+                description={__('Enable this option to use WordPress Reading Homepage Setting instead of themes front-page template..', 'gutenverse')}
+                value={template_page.use_setting_homepage}
+                updateValue={updateValue}
+            />
+        </div>
+        <div className="template-tab-body">
+            <ControlCheckbox
                 id={'inherit_layout'}
                 title={__('Inherit Layout', 'gutenverse')}
                 description={__('Section\'s width inside post content will inherit your theme layout content size.', 'gutenverse')}
