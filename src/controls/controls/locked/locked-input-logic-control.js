0f4ac1e3a8a519dacd99dceadb2f2976caa4764c
Arya1Putra <putra1arya@gmail.com>
2023-09-06 15:47:12 -0700

    add video for logic form

--- src/controls/controls/locked/locked-input-logic-control.js
+++ src/controls/controls/locked/locked-input-logic-control.js
@@ -3,14 +3,15 @@ import { useInstanceId } from '@wordpress/compose';
 import { __ } from '@wordpress/i18n';
 import DefaultLayout from './default-layout';
 
-const LockedInputLogicControl = () => {
+const LockedInputLogicControl = ({isOpen}) => {
     const id = useInstanceId(LockedInputLogicControl, 'inspector-locked-input-logic-control');
 
     return <div id={id} className={'gutenverse-control-wrapper gutenverse-control-locked-input-logic gutenverse-control-locked-layout'}>
         <DefaultLayout
             title={__( 'Input Logic', '--gctd--' )}
             description={__( 'Input Logic allows you to create a conditional display for certain input values.', '--gctd--' )}
-            img={'pro/sticky.gif'}
+            img={'pro/video/form-logic.mp4'}
+            isOpen={isOpen}
         />
     </div>;
 };
