88bdc948b9a0db57cdf06c84078614b32f4baf7d
Arya1Putra <putra1arya@gmail.com>
2023-09-07 18:06:34 +0800

    add input file for costum file

--- src/backend/forms/input-file.js
+++ src/backend/forms/input-file.js
@@ -2,7 +2,7 @@
 import { v4 as uuidv4 } from 'uuid';
 
 const ControlFile = (props) => {
-    const { id, title, description, value, defaultValue = '', updateValue, isRequired = false } = props;
+    const { id, title, description, value, defaultValue = '',customLabel, updateValue, isRequired = false } = props;
     let uuid = uuidv4();
 
     const inputChange = (e) => {
@@ -12,7 +12,7 @@ const ControlFile = (props) => {
     const inputValue = value === undefined ? defaultValue : value;
 
     return <div className="control-wrapper control-text">
-        <label className="control-title" htmlFor={`${id}-${uuid}`}>{title} {isRequired && <span style={{color:'red'}}> *</span>}</label>
+        <label className="control-title" htmlFor={`${id}-${uuid}`} style={customLabel}>{title} {isRequired && <span style={{color:'red'}}> *</span>}</label>
         <input id={`${id}-${uuid}`} type="file" value={inputValue} accept=".woff, .woff2, .ttf, .svg"  onChange={inputChange} />
         {description !== '' && <span className="control-description">
             {description}
