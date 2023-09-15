9407978908fbe2f76f56c64237c855fb6d0b3e6d
Arya1Putra <putra1arya@gmail.com>
2023-09-15 12:06:16 +0800

    fix bug inline progressbar by removing option inline in width

--- src/editor/blocks/progress-bar/panels/panel-list.js
+++ src/editor/blocks/progress-bar/panels/panel-list.js
@@ -61,7 +61,23 @@ export const panelList = () => {
         {
             title: __('Positioning', 'gutenverse'),
             initialOpen: false,
-            panelArray: positioningPanel,
+            panelArray: (props) => positioningPanel({
+                ...props,
+                options: [
+                    {
+                        value: 'default',
+                        label: 'Default'
+                    },
+                    {
+                        value: 'full',
+                        label: 'Full Width (100%)'
+                    },
+                    {
+                        value: 'custom',
+                        label: 'Custom'
+                    }
+                ]
+            }),
             tabRole: TabSetting
         },
         {
