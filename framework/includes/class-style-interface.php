4d855f97bbf45c939e449afdc91f827ea9d626e7
Arya1Putra <putra1arya@gmail.com>
2023-09-21 11:09:07 +0800

    fix display hide in editor

--- framework/includes/class-style-interface.php
+++ framework/includes/class-style-interface.php
@@ -586,11 +586,11 @@ abstract class Style_Interface {
 					'selector'       => $selector,
 					'property'       => function( $value, $device ) {
 						$output  = '';
-						$display = 'display: inline-block';
+						$display = 'display: inline-block;';
 
 						if ( ! empty( $value['hide'] ) ) {
-							$display = 'display: inline-block';
-							$output  = 'display: inline-block';
+							$display = 'display: inline-block;';
+							$output  = '';
 						}
 						
 						if ( isset( $value ) && isset( $value['type'] ) ) {
