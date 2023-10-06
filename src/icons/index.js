4b5be1b52b1e7e4af6055da3e2a82608e9a599fc
Arya1Putra <putra1arya@gmail.com>
2023-10-06 11:59:41 +0800

    fix typo

--- src/icons/index.js
+++ src/icons/index.js
@@ -39,7 +39,7 @@ import IconToolbarColumnAdd from './icon/icon-toolbar-column-add.svg';
 import IconToolbarColumnDelete from './icon/icon-toolbar-column-delete.svg';
 import IconFont from './icon/icon-font.svg';
 import IconDownload from './icon/icon-download.svg';
-import IconAddFont form './icon/icon-add-font.svg';
+import IconAddFont from './icon/icon-add-font.svg';
 
 /* Block */
 import IconColumn from './icon/icon-column.svg';
