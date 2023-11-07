87616f1fdf2d2d427fc0d3f2082311715bf8f1be
Arya1Putra <putra1arya@gmail.com>
2023-11-07 11:07:12 +0800

    adding block archive title

---
+++ src/editor/blocks/archive-title/panels/panel-list.js
@@ -0,0 +1,89 @@
+/* WordPress dependencies */
+import { __ } from '@wordpress/i18n';
+import { advancePanel, animationPanel, backgroundPanel, borderPanel, maskPanel, positioningPanel, responsivePanel, transformPanel } from 'gutenverse-core/controls';
+import { settingPanel } from './panel-setting';
+// import { settingPanelDeprecated } from './panel-setting-deprecated';
+import { stylePanel } from './panel-style';
+import { TabSetting, TabStyle } from 'gutenverse-core/controls';
+
+export const panelList = () => {
+    return [
+        {
+            title: __('Setting', 'gutenverse'),
+            panelArray: settingPanel,
+            tabRole: TabSetting
+        },
+        /* {
+            title: __('Setting (Deprecated)', 'gutenverse'),
+            panelArray: settingPanelDeprecated
+        }, */
+        {
+            title: __('Style', 'gutenverse'),
+            initialOpen: false,
+            panelArray: stylePanel,
+            tabRole: TabStyle
+        },
+        {
+            title: __('Background', 'gutenverse'),
+            initialOpen: false,
+            panelArray: (props) => backgroundPanel({
+                ...props,
+                styleId: 'archive-title-background',
+                normalOptions: [ 'default', 'gradient' ],
+                hoverOptions: [ 'default', 'gradient' ],
+            }),
+            tabRole: TabStyle
+        },
+        {
+            title: __('Border', 'gutenverse'),
+            initialOpen: false,
+            panelArray: (props) => borderPanel({
+                ...props,
+                styleId: 'archive-title-border',
+            }),
+            tabRole: TabStyle
+        },
+        {
+            title: __('Masking', 'gutenverse'),
+            initialOpen: false,
+            panelArray: maskPanel,
+            tabRole: TabStyle
+        },
+        {
+            title: __('Display', 'gutenverse'),
+            initialOpen: false,
+            panelArray: responsivePanel,
+            tabRole: TabSetting
+        },
+        {
+            title: __('Positioning', 'gutenverse'),
+            initialOpen: false,
+            panelArray: positioningPanel,
+            tabRole: TabSetting
+        },
+        {
+            title: __('Animation Effects', 'gutenverse'),
+            initialOpen: false,
+            panelArray: (props) => animationPanel({
+                ...props,
+                styleId: 'archive-title-animation'
+            }),
+            tabRole: TabSetting
+        },
+        {
+            title: __('Transform', 'gutenverse'),
+            initialOpen: false,
+            panelArray: transformPanel,
+            pro: true
+        },
+        {
+            title: __('Spacing', 'gutenverse'),
+            initialOpen: false,
+            panelArray: (props) => advancePanel({
+                ...props,
+                styleId: 'archive-title-advance',
+            }),
+            tabRole: TabSetting
+        }
+    ];
+};
