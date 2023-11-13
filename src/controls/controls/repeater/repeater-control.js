79737cfb82af0e9e0d56035afd68a09378ea7f5e
Arya1Putra <putra1arya@gmail.com>
2023-11-13 16:52:05 +0800

    add span id

--- src/controls/controls/repeater/repeater-control.js
+++ src/controls/controls/repeater/repeater-control.js
@@ -44,7 +44,7 @@ const RepeaterComponent = (props) => {
         if (style) {
             const theStyle = style.map(item => {
                 const { selector } = item;
-                let theSelector = typeof selector === 'string' || selector instanceof String ? selector : selector(repeaterIndex, props.value.id);
+                let theSelector = typeof selector === 'string' || selector instanceof String ? selector : selector(repeaterIndex, props.value.id, props.value.spanId);
                 return {
                     ...item,
                     selector: theSelector
