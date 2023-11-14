0be85d97d7067faa4099f83682c1879b1aec938a
Yoga <yoga@jegstudio.com>
2023-11-14 15:44:48 +0800

    add bg animation to column

--- src/blocks/column/edit.js
+++ src/blocks/column/edit.js
@@ -2,11 +2,11 @@ import { useRef, useState, useEffect } from '@wordpress/element';
 import { InnerBlocks, useBlockProps, Inserter, BlockControls } from '@wordpress/block-editor';
 import classnames from 'classnames';
 import { compose } from '@wordpress/compose';
-import { withCustomStyle, withCopyElementToolbar, withAnimationSticky } from 'gutenverse-core/hoc';
+import { withCustomStyle, withCopyElementToolbar, withAnimationSticky, withAnimationBackground } from 'gutenverse-core/hoc';
 import { panelList } from './panels/panel-list';
 import { PanelController } from 'gutenverse-core/controls';
 import { BuildColumnWidthStyle, setDeviceClasses } from 'gutenverse-core/styling';
-import { isSticky } from 'gutenverse-core/helper';
+import { isAnimationActive, isSticky } from 'gutenverse-core/helper';
 import { getDeviceType } from 'gutenverse-core/editor-helper';
 import { dispatch, select, useSelect } from '@wordpress/data';
 import { useAnimationEditor, useDisplayEditor } from 'gutenverse-core/hooks';
@@ -529,8 +529,12 @@ const ColumnWrapper = (props) => {
     } = props;
 
     const {
+        elementId,
         width,
+        backgroundAnimated = {}
     } = attributes;
+
+    const dataId = elementId ? elementId.split('-')[1] : '';
 
     const wvalue = width[deviceType] ? width[deviceType] : width['Desktop'];
 
@@ -617,6 +621,7 @@ const ColumnWrapper = (props) => {
                 </div>
                 <div className={'sticky-wrapper'} ref={stickyFlagRef}>
                     <div className={wrapperClass} ref={columnWrapRef}>
+                        {isAnimationActive(backgroundAnimated) && <div className={'guten-background-animated'}><div className={`animated-layer animated-${dataId}`}></div></div>}
                         <InnerBlocks />
                     </div>
                 </div>
@@ -776,6 +781,7 @@ const ColumnBlockControl = (props) => {
 // Column Block edit component
 const ColumnBlock = compose(
     withCustomStyle(panelList),
+    withAnimationBackground(),
     withAnimationSticky(),
     withCopyElementToolbar()
 )((props) => {
@@ -812,6 +818,7 @@ const ColumnBlock = compose(
         width,
         sticky = {},
         stickyPosition,
+        backgroundAnimated = {},
     } = attributes;
 
     const hasChildBlocks = getBlockOrder(clientId).length > 0;
@@ -949,6 +956,7 @@ const ColumnBlock = compose(
                 'column-filled': hasChildBlocks,
                 [`sticky-${stickyPosition}`]: isSticky(sticky),
                 'is-hovered': isHovered,
+                'background-animated': isAnimationActive(backgroundAnimated),
             }
         ),
         ref: columnRef,
--- src/blocks/column/panels/panel-list.js
+++ src/blocks/column/panels/panel-list.js
@@ -1,6 +1,6 @@
 import { __ } from '@wordpress/i18n';
 import { layoutPanel } from './panel-layout';
-import { advancePanel, animationPanel, backgroundPanel, backgroundOverlayPanel, borderPanel, responsivePanel, typographyPanel, maskPanel } from 'gutenverse-core/controls';
+import { advancePanel, animationPanel, backgroundPanel, backgroundOverlayPanel, borderPanel, responsivePanel, typographyPanel, maskPanel, backgroundAnimatedPanel } from 'gutenverse-core/controls';
 import { stickyPanel } from './panel-sticky';
 import { TabSetting, TabStyle } from 'gutenverse-core/controls';
 import { blurPanel } from './panel-blur';
@@ -26,8 +26,8 @@ export const panelList = () => {
                 styleId: 'column-background',
                 normalOptions: [ 'default', 'gradient' ],
                 hoverOptions: [ 'default', 'gradient' ],
-                normalSelector: `.editor-styles-wrapper .is-root-container .${props.elementId} > .guten-column-resizeable > .sticky-wrapper > .guten-column-wrapper`,
-                hoverSelector: `.editor-styles-wrapper .is-root-container .${props.elementId} > .guten-column-resizeable > .sticky-wrapper > .guten-column-wrapper:hover`
+                normalSelector: `.editor-styles-wrapper .is-root-container .${props.elementId}:not(.background-animated) > .guten-column-resizeable > .sticky-wrapper > .guten-column-wrapper, .${props.elementId}.background-animated > .guten-column-resizeable > .sticky-wrapper > .guten-column-wrapper > .guten-background-animated .animated-layer`,
+                hoverSelector: `.editor-styles-wrapper .is-root-container .${props.elementId}:not(.background-animated) > .guten-column-resizeable > .sticky-wrapper > .guten-column-wrapper:hover, .${props.elementId}.background-animated > .guten-column-resizeable > .sticky-wrapper > .guten-column-wrapper:hover > .guten-background-animated .animated-layer`
             }),
             tabRole: TabStyle
         },
@@ -74,6 +74,12 @@ export const panelList = () => {
                 styleId: 'column-typography'
             }),
             tabRole: TabStyle
+        },
+        {
+            title: __('Background Animated', '--gctd--'),
+            initialOpen: false,
+            panelArray: backgroundAnimatedPanel,
+            pro: true,
         },
         {
             title: __('Display', '--gctd--'),
