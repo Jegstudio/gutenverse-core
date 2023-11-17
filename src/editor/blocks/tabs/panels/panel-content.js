1e5621831699359fd5657289e8de83f3c9e2610d
Arya1Putra <putra1arya@gmail.com>
2023-11-17 10:10:50 +0800

    fix tag vertical

--- src/editor/blocks/tabs/panels/panel-content.js
+++ src/editor/blocks/tabs/panels/panel-content.js
@@ -1,34 +1,20 @@
 import { SelectControl} from 'gutenverse-core/controls';
 import { __ } from '@wordpress/i18n';
 import { select } from '@wordpress/data';
-import { getDeviceType } from 'gutenverse-core/editor-helper';
 import isEmpty from 'lodash/isEmpty';
-import { handleUnitPoint, deviceStyleValue } from 'gutenverse-core/styling';
 
 
 export const contentPanel = (props) => {
     const {
         clientId,
         elementId,
-        positioningWidth,
         selector,
-        inFlex = true,
     } = props;
     const blockName = select('core/block-editor').getBlockName(clientId);
-    const deviceType = getDeviceType();
     const checkSelector = !isEmpty(selector) ? selector : `.${elementId}.guten-element`;
     const customSelector = blockName !== 'gutenverse/section' ? checkSelector : `.section-wrapper[data-id="${elementId?.split('-')[1]}"]`;
-    const setPositioning = (width=false) => {
-        const value = 'custom';
-        switch (value) {
-            case 'full':
-                return 'width: 100%!important;';
-            case 'inline':
-                return `width: auto!important; display: ${ inFlex ? 'inline-block' : 'inline-flex' }!important;`;
-            case 'custom':
-                return `${handleUnitPoint(width, 'width', true)} display: ${ inFlex ? 'inline-block' : 'inline-flex' }!important;`;
-        }
-    };
+
+   
     return [
         {
             id: 'orientation',
@@ -60,9 +46,25 @@ export const contentPanel = (props) => {
             style: [
                 {
                     selector: customSelector,
-                    updateID: 'positioningWidth-style-0',
                     allowRender: value => value,
-                    render: () => setPositioning(deviceStyleValue(deviceType, positioningWidth))
+                    render : value => {
+                        if(value === 'horizontal'){
+                            return 'display: block;'
+                        }else if(value === 'vertical'){
+                            return 'display: flex;'
+                        }
+                    }
+                },
+                {
+                    selector: `${checkSelector} .tab-heading-item` ,
+                    allowRender: value => value,
+                    render : value => {
+                        if(value === 'horizontal-center'){
+                            return 'justify-content: center;'
+                        }else if(value === 'horizontal-right'){
+                            return 'justify-content: end;'
+                        }
+                    }
                 }
             ]
         },
