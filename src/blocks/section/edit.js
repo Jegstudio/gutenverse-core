1c1ce6f3ee206a4fa1d2405ddd5875532a082d43
Agung Bayu <agungbayuiswara@gmail.com>
2023-12-18 15:06:35 +0800

    Fix fluid canvas section

--- src/blocks/section/edit.js
+++ src/blocks/section/edit.js
@@ -266,8 +266,8 @@ const SectionBlock = compose(
         />
         <SectionInspection {...props} />
         <div id={dataId} className={`guten-section-wrapper section-wrapper section-${elementId} sticky-${stickyPosition} ${inheritLayout ? 'inherit-layout' : ''} ${cursorEffect?.show ? 'guten-cursor-effect' : ''}`} ref={sectionWrapper} data-id={dataId}>
-            <FluidCanvas attributes={attributes} />
             <section {...blockProps}>
+                <FluidCanvas attributes={attributes} />
                 {isAnimationActive(backgroundAnimated) && <div className={'guten-background-animated'}><div className={`animated-layer animated-${dataId}`}></div></div>}
                 <SectionVideoContainer {...props} />
                 <div className="guten-background-overlay" />
