0d538f3d6297b54806be9e7a9a16972d01d8f640
Arya1Putra <putra1arya@gmail.com>
2024-01-03 17:44:44 +0800

    section deprecated

--- src/blocks/section/index.js
+++ src/blocks/section/index.js
@@ -4,6 +4,7 @@ import save from './save';
 import saveV1 from './deprecated/v1/save';
 import saveV2 from './deprecated/v2/save';
 import saveV3 from './deprecated/v3/save';
+import saveV4 from './deprecated/v4/save';
 import attrV2 from './deprecated/v2/attribute.json';
 import metadata from './block.json';
 import example from './data/example';
@@ -22,6 +23,10 @@ export const settings = {
     edit,
     save,
     deprecated: [
+        {
+            attributes,
+            save: saveV4
+        },
         {
             attributes,
             save: saveV3
