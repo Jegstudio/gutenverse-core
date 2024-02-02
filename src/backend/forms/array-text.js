09e148480c87e073e6e5675125b12e4de4b89fb9
Arya1Putra <putra1arya@gmail.com>
2024-02-02 18:12:14 +0800

    form affiliate

--- src/backend/forms/array-text.js
+++ src/backend/forms/array-text.js
@@ -1,27 +1,42 @@
 import { v4 as uuidv4 } from 'uuid';
 
 const ControlArrayText = (props) => {
-    const { id, title, description, value, defaultValue = [], updateValue, customLabel, isRequired } = props;
+    const { id, title, description, value, defaultValue = [], updateValue, updateArrayValue, customLabel, isRequired } = props;
+    console.log(value);
     let uuid = uuidv4();
     const inputValue = value === undefined ? defaultValue : value;
     const addClicked = () => {
-        inputValue.push('');
+        const newArr = inputValue
+        newArr.push('');
+        updateValue(id,newArr);
     };
-    const inputChange = (e) => {
-        updateValue(id, e);
+    const inputChange = (e,index) => {
+        const target = e.target.value;
+        updateArrayValue(id, index, target);
     };
 
 
     return <div className="control-wrapper control-array-text">
         <label className="control-title" htmlFor={`${id}-${uuid}`} style={customLabel}>{title} {isRequired && <span style={{color:'red'}}> *</span>}</label>
         {
-            value.map((el,index) => {
-                index !== (value.length - 1) ? <input id={`${id}-${uuid}`} type="text" value={val}  placeholder='https://www.example.com'/> : <div className="input-array-field">
-                    <input id={`${id}-${uuid}`} type="text" value={val} placeholder='https://www.example.com'/>
-                    <button onClick={addClicked}>+</button>
-                </div>
+            value && value.map((el,index) => {
+
+                return <div className="input-array-field" key={uuid}>
+                    <input
+                        id={`${id}-${uuid}`}
+                        type="text"
+                        value={el}
+                        onChange={(e) => inputChange(e,index)}
+                        placeholder='https://www.example.com'
+                    />
+                    {index === value.length - 1 && <div onClick={addClicked}>+</div>}
+                </div>;
+                // return index !== (value.length - 1) ? <input id={`${id}-${uuid}`} key={uuid} type="text" value={el}  placeholder='https://www.example.com' onChange={(e) => inputChange(e,index)}/> : <div className="input-array-field" key={uuid}>
+                //     <input id={`${id}-${uuid}`} type="text" value={el} onChange={(e) => inputChange(e,index)}placeholder='https://www.example.com'/>
+                // </div>
             })
         }
+        
         
         
         {description !== '' && <span className="control-description">
