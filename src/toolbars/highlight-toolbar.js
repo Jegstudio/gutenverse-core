d0d32286e00f62bf3c48b94ba8466b57db3e12a6
Gowida <gowinda@jegstudio.com>
2024-02-22 18:06:12 +0800

    second day test

--- src/toolbars/highlight-toolbar.js
+++ src/toolbars/highlight-toolbar.js
@@ -1,31 +1,53 @@
 import { registerFormatType, toggleFormat } from '@wordpress/rich-text';
 import { BlockControls } from '@wordpress/block-editor';
 import { ToolbarGroup, ToolbarButton } from '@wordpress/components';
+import { useSelect } from '@wordpress/data';
+import { useState,useEffect } from '@wordpress/element';
 
-export const HighlightButton = ( { isActive, onChange, value } ) => {
+import { cryptoRandomString } from 'gutenverse-core/components';
+
+var UNIQUE_ID = 'guten-' + cryptoRandomString({ length: 6, type: 'alphanumeric' });
+
+export const HighlightButton = ({ isActive, onChange, value }) => {
+    const selectedBlock = useSelect( ( select ) => {
+        return select( 'core/block-editor' ).getSelectedBlock();
+    }, [] );
+
+    const [test, setTest] = useState(true);
+
+    useEffect(()=>{
+        console.log(selectedBlock);
+    },[test]);
+
+    // if ( selectedBlock && selectedBlock.name !== 'core/paragraph' ) {
+    //     return null;
+    // }
+
     return (
         <BlockControls>
             <ToolbarGroup>
                 <ToolbarButton
                     icon="editor-code"
                     title="Text Highlight"
-                    onClick={ () => {
+                    onClick={() => {
+                        setTest(!test);
+                        UNIQUE_ID = 'guten-' + cryptoRandomString({ length: 6, type: 'alphanumeric' });
                         onChange(
-                            toggleFormat( value, {
+                            toggleFormat(value, {
                                 type: 'highlight-format/text-highlight',
-                            } )
+                            })
                         );
-                    } }
-                    isActive={ isActive }
+                    }}
+                    isActive={isActive}
                 />
             </ToolbarGroup>
         </BlockControls>
     );
 };
 
-registerFormatType( 'highlight-format/text-highlight', {
+registerFormatType('highlight-format/text-highlight', {
     title: 'Text Highlight',
     tagName: 'span',
-    className: null,
     edit: HighlightButton,
-} );
+    className: `${UNIQUE_ID}`,
+});
