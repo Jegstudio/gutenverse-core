59b72e82ead4581f6b1bf78d9a8de2e8aab239c9
Arya1Putra <putra1arya@gmail.com>
2024-03-25 12:13:20 +0800

    add panel position to add open panel

--- src/hoc/with-high-light-text.js
+++ src/hoc/with-high-light-text.js
@@ -1,11 +1,11 @@
 import { applyFilters } from '@wordpress/hooks';
 
-export const withHighLightText =  (contentAttribute) => (BlockElement) => {
+export const withHighLightText =  (contentAttribute, panelPosition) => (BlockElement) => {
     return (props) => {
         return applyFilters(
             'gutenverse.hoc.high-light-text',
             <BlockElement {...props} />,
-            { BlockElement, props, contentAttribute }
+            { BlockElement, props, contentAttribute, panelPosition }
         );
     };
 };
