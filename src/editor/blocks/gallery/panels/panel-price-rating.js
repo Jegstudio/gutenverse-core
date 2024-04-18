d46a0b4b002f31c05bb84668ce75cc0c5024f669
Arya1Putra <putra1arya@gmail.com>
2024-04-18 18:02:15 +0800

    [fix/add-feature] rating and price galerry positioning and margin padding custom

--- src/editor/blocks/gallery/panels/panel-price-rating.js
+++ src/editor/blocks/gallery/panels/panel-price-rating.js
@@ -1,16 +1,119 @@
 import { __ } from '@wordpress/i18n';
-import { ColorControl, DimensionControl, HeadingControl, TypographyControl } from 'gutenverse-core/controls';
-import { handleColor, handleDimension, handleTypography } from 'gutenverse-core/styling';
+import { ColorControl, DimensionControl, HeadingControl, SelectControl, SizeControl, TypographyControl } from 'gutenverse-core/controls';
+import { handleColor, handleDimension, handleTypography, handleUnitPoint } from 'gutenverse-core/styling';
 
 export const priceRatingPanel = (props) => {
     const {
-        elementId
+        elementId,
+        selectionPriceRatingPadding
     } = props;
 
     return [
         {
+            id: 'selectionPriceRatingPadding',
+            label: __('Select Setting Price Rating Padding', 'gutenverse'),
+            component: SelectControl,
+            options: [
+                {
+                    value: 'all',
+                    label: __('All in One')
+                },
+                {
+                    value: 'custom',
+                    label: __('Custom')
+                },
+            ],
+        },
+        {
             id: 'priceRatingPadding',
             label: __('Padding', 'gutenverse'),
+            component: DimensionControl,
+            position: ['top', 'right', 'bottom', 'left'],
+            allowDeviceControl: true,
+            show: selectionPriceRatingPadding === 'all',
+            units: {
+                px: {
+                    text: 'px',
+                    unit: 'px'
+                },
+                em: {
+                    text: 'em',
+                    unit: 'em'
+                },
+                ['%']: {
+                    text: '%',
+                    unit: '%'
+                },
+            },
+            style: [
+                {
+                    selector: `.${elementId} .gallery-items .gallery-item-wrap .grid-item .caption-wrap .caption-head`,
+                    allowRender : () => selectionPriceRatingPadding === 'all',
+                    render: value => handleDimension(value, 'padding')
+                }
+            ]
+        },
+        {
+            id: 'pricePadding',
+            label: __('Price Padding', 'gutenverse'),
+            component: DimensionControl,
+            position: ['top', 'right', 'bottom', 'left'],
+            allowDeviceControl: true,
+            show: selectionPriceRatingPadding === 'custom',
+            units: {
+                px: {
+                    text: 'px',
+                    unit: 'px'
+                },
+                em: {
+                    text: 'em',
+                    unit: 'em'
+                },
+                ['%']: {
+                    text: '%',
+                    unit: '%'
+                },
+            },
+            style: [
+                {
+                    selector: `.${elementId} .gallery-items .gallery-item-wrap .grid-item .caption-wrap .caption-head .item-price`,
+                    allowRender: () => selectionPriceRatingPadding === 'custom',
+                    render: value => handleDimension(value, 'padding')
+                }
+            ]
+        },
+        {
+            id: 'ratingPadding',
+            label: __('Rating Padding', 'gutenverse'),
+            component: DimensionControl,
+            position: ['top', 'right', 'bottom', 'left'],
+            allowDeviceControl: true,
+            show: selectionPriceRatingPadding === 'custom',
+            units: {
+                px: {
+                    text: 'px',
+                    unit: 'px'
+                },
+                em: {
+                    text: 'em',
+                    unit: 'em'
+                },
+                ['%']: {
+                    text: '%',
+                    unit: '%'
+                },
+            },
+            style: [
+                {
+                    selector: `.${elementId} .gallery-items .gallery-item-wrap .grid-item .caption-wrap .caption-head .item-rating`,
+                    allowRender: () => selectionPriceRatingPadding === 'custom',
+                    render: value => handleDimension(value, 'padding')
+                }
+            ]
+        },
+        {
+            id: 'priceMargin',
+            label: __('Price Margin', 'gutenverse'),
             component: DimensionControl,
             position: ['top', 'right', 'bottom', 'left'],
             allowDeviceControl: true,
@@ -30,9 +133,372 @@ export const priceRatingPanel = (props) => {
             },
             style: [
                 {
-                    selector: `.${elementId} .gallery-items .gallery-item-wrap .grid-item .caption-wrap .caption-head`,
-                    render: value => handleDimension(value, 'padding')
+                    selector: `.${elementId} .gallery-items .gallery-item-wrap .grid-item .caption-wrap .caption-head .item-price`,
+                    render: value => handleDimension(value, 'margin')
                 }
+            ]
+        },
+        {
+            id: 'ratingMargin',
+            label: __('Rating Margin', 'gutenverse'),
+            component: DimensionControl,
+            position: ['top', 'right', 'bottom', 'left'],
+            allowDeviceControl: true,
+            units: {
+                px: {
+                    text: 'px',
+                    unit: 'px'
+                },
+                em: {
+                    text: 'em',
+                    unit: 'em'
+                },
+                ['%']: {
+                    text: '%',
+                    unit: '%'
+                },
+            },
+            style: [
+                {
+                    selector: `.${elementId} .gallery-items .gallery-item-wrap .grid-item .caption-wrap .caption-head .item-rating`,
+                    render: value => handleDimension(value, 'margin')
+                }
+            ]
+        },
+        {
+            id: 'pricePositioningLeft',
+            label: __('Price Left Orientation', '--gctd--'),
+            component: SizeControl,
+            allowDeviceControl: true,
+            units: {
+                px: {
+                    text: 'px',
+                    min: -1000,
+                    max: 1000,
+                    step: 1,
+                    unit: 'px',
+                },
+                ['%']: {
+                    text: '%',
+                    min: -200,
+                    max: 200,
+                    step: 1,
+                    unit: '%',
+                },
+                vw: {
+                    text: 'vw',
+                    min: -200,
+                    max: 200,
+                    step: 1,
+                    unit: 'vw',
+                },
+                vh: {
+                    text: 'vw',
+                    min: -200,
+                    max: 200,
+                    step: 1,
+                    unit: 'vw',
+                },
+            },
+            style: [
+                {
+                    selector: `.${elementId} .gallery-items .gallery-item-wrap .grid-item .caption-wrap .caption-head .item-price`,
+                    render: value => handleUnitPoint(value, 'left')
+                },
+            ]
+        },
+        {
+            id: 'pricePositioningRight',
+            label: __('Right Orientation', '--gctd--'),
+            component: SizeControl,
+            allowDeviceControl: true,
+            units: {
+                px: {
+                    text: 'px',
+                    min: -1000,
+                    max: 1000,
+                    step: 1,
+                    unit: 'px',
+                },
+                ['%']: {
+                    text: '%',
+                    min: -200,
+                    max: 200,
+                    step: 1,
+                    unit: '%',
+                },
+                vw: {
+                    text: 'vw',
+                    min: -200,
+                    max: 200,
+                    step: 1,
+                    unit: 'vw',
+                },
+                vh: {
+                    text: 'vw',
+                    min: -200,
+                    max: 200,
+                    step: 1,
+                    unit: 'vw',
+                },
+            },
+            style: [
+                {
+                    selector: `.${elementId} .gallery-items .gallery-item-wrap .grid-item .caption-wrap .caption-head .item-price`,
+                    render: value => handleUnitPoint(value, 'right')
+                },
+            ]
+        },
+        {
+            id: 'pricePositioningTop',
+            label: __('Top Orientation', '--gctd--'),
+            component: SizeControl,
+            allowDeviceControl: true,
+            units: {
+                px: {
+                    text: 'px',
+                    min: -1000,
+                    max: 1000,
+                    step: 1,
+                    unit: 'px',
+                },
+                ['%']: {
+                    text: '%',
+                    min: -200,
+                    max: 200,
+                    step: 1,
+                    unit: '%',
+                },
+                vw: {
+                    text: 'vw',
+                    min: -200,
+                    max: 200,
+                    step: 1,
+                    unit: 'vw',
+                },
+                vh: {
+                    text: 'vw',
+                    min: -200,
+                    max: 200,
+                    step: 1,
+                    unit: 'vw',
+                },
+            },
+            style: [
+                {
+                    selector: `.${elementId} .gallery-items .gallery-item-wrap .grid-item .caption-wrap .caption-head .item-price`,
+                    render: value => handleUnitPoint(value, 'top'),
+                },
+            ]
+        },
+        {
+            id: 'pricePositioningBottom',
+            label: __('Price Bottom Orientation', '--gctd--'),
+            component: SizeControl,
+            allowDeviceControl: true,
+            units: {
+                px: {
+                    text: 'px',
+                    min: -1000,
+                    max: 1000,
+                    step: 1,
+                    unit: 'px',
+                },
+                ['%']: {
+                    text: '%',
+                    min: -200,
+                    max: 200,
+                    step: 1,
+                    unit: '%',
+                },
+                vw: {
+                    text: 'vw',
+                    min: -200,
+                    max: 200,
+                    step: 1,
+                    unit: 'vw',
+                },
+                vh: {
+                    text: 'vw',
+                    min: -200,
+                    max: 200,
+                    step: 1,
+                    unit: 'vw',
+                },
+            },
+            style: [
+                {
+                    selector: `.${elementId} .gallery-items .gallery-item-wrap .grid-item .caption-wrap .caption-head .item-price`,
+                    render: value => handleUnitPoint(value, 'bottom'),
+                },
+            ]
+        },
+        {
+            id: 'ratingPositioningLeft',
+            label: __('Rating Left Orientation', '--gctd--'),
+            component: SizeControl,
+            allowDeviceControl: true,
+            units: {
+                px: {
+                    text: 'px',
+                    min: -1000,
+                    max: 1000,
+                    step: 1,
+                    unit: 'px',
+                },
+                ['%']: {
+                    text: '%',
+                    min: -200,
+                    max: 200,
+                    step: 1,
+                    unit: '%',
+                },
+                vw: {
+                    text: 'vw',
+                    min: -200,
+                    max: 200,
+                    step: 1,
+                    unit: 'vw',
+                },
+                vh: {
+                    text: 'vw',
+                    min: -200,
+                    max: 200,
+                    step: 1,
+                    unit: 'vw',
+                },
+            },
+            style: [
+                {
+                    selector: `.${elementId} .gallery-items .gallery-item-wrap .grid-item .caption-wrap .caption-head .item-rating`,
+                    render: value => handleUnitPoint(value, 'left')
+                },
+            ]
+        },
+        {
+            id: 'ratingPositioningRight',
+            label: __('Rating Right Orientation', '--gctd--'),
+            component: SizeControl,
+            allowDeviceControl: true,
+            units: {
+                px: {
+                    text: 'px',
+                    min: -1000,
+                    max: 1000,
+                    step: 1,
+                    unit: 'px',
+                },
+                ['%']: {
+                    text: '%',
+                    min: -200,
+                    max: 200,
+                    step: 1,
+                    unit: '%',
+                },
+                vw: {
+                    text: 'vw',
+                    min: -200,
+                    max: 200,
+                    step: 1,
+                    unit: 'vw',
+                },
+                vh: {
+                    text: 'vw',
+                    min: -200,
+                    max: 200,
+                    step: 1,
+                    unit: 'vw',
+                },
+            },
+            style: [
+                {
+                    selector: `.${elementId} .gallery-items .gallery-item-wrap .grid-item .caption-wrap .caption-head .item-rating`,
+                    render: value => handleUnitPoint(value, 'right')
+                },
+            ]
+        },
+        {
+            id: 'ratingPositioningTop',
+            label: __('Rating Top Orientation', '--gctd--'),
+            component: SizeControl,
+            allowDeviceControl: true,
+            units: {
+                px: {
+                    text: 'px',
+                    min: -1000,
+                    max: 1000,
+                    step: 1,
+                    unit: 'px',
+                },
+                ['%']: {
+                    text: '%',
+                    min: -200,
+                    max: 200,
+                    step: 1,
+                    unit: '%',
+                },
+                vw: {
+                    text: 'vw',
+                    min: -200,
+                    max: 200,
+                    step: 1,
+                    unit: 'vw',
+                },
+                vh: {
+                    text: 'vw',
+                    min: -200,
+                    max: 200,
+                    step: 1,
+                    unit: 'vw',
+                },
+            },
+            style: [
+                {
+                    selector: `.${elementId} .gallery-items .gallery-item-wrap .grid-item .caption-wrap .caption-head .item-rating`,
+                    render: value => handleUnitPoint(value, 'top'),
+                },
+            ]
+        },
+        {
+            id: 'ratingPositioningBottom',
+            label: __('Rating Bottom Orientation', '--gctd--'),
+            component: SizeControl,
+            allowDeviceControl: true,
+            units: {
+                px: {
+                    text: 'px',
+                    min: -1000,
+                    max: 1000,
+                    step: 1,
+                    unit: 'px',
+                },
+                ['%']: {
+                    text: '%',
+                    min: -200,
+                    max: 200,
+                    step: 1,
+                    unit: '%',
+                },
+                vw: {
+                    text: 'vw',
+                    min: -200,
+                    max: 200,
+                    step: 1,
+                    unit: 'vw',
+                },
+                vh: {
+                    text: 'vw',
+                    min: -200,
+                    max: 200,
+                    step: 1,
+                    unit: 'vw',
+                },
+            },
+            style: [
+                {
+                    selector: `.${elementId} .gallery-items .gallery-item-wrap .grid-item .caption-wrap .caption-head .item-rating`,
+                    render: value => handleUnitPoint(value, 'bottom'),
+                },
             ]
         },
         {
