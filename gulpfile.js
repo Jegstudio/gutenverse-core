73eb24b866f4b3aaf3ea2e37a31fdac6f9dff005
Gowinda Kadek <gowinda@jegstudio.com>
2024-04-25 09:42:27 +0800

    [dev/draft-library] remove gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -108,13 +108,6 @@ gulp.task('replace-text-domain', function () {
         .pipe(gulp.dest('./release/gutenverse/lib/framework/'));
 });
 
-gulp.task('use-dev-mode', function () {
-    return gulp
-        .src(['./release/gutenverse/lib/framework/**/*.js', './release/gutenverse/lib/framework/**/*.php'])
-        .pipe(replace('--dev_mode--', 'true'))
-        .pipe(gulp.dest('./release/gutenverse/lib/framework/'));
-});
-
 gulp.task('release', gulp.series(
     'copy-plugin-folder',
     'copy-framework',
