7e4d81a025789cfc3d695c76a7d9aad004e8063c
Arya1Putra <putra1arya@gmail.com>
2024-05-13 13:50:24 +0800

    [fix/fix-bug] fix icon deprecated

---
+++ src/editor/blocks/icon/deprecated/v1/save.js
@@ -0,0 +1,71 @@
+import { compose } from '@wordpress/compose';
+import { useBlockProps } from '@wordpress/block-editor';
+import { classnames } from 'gutenverse-core/components';
+import { applyFilters } from '@wordpress/hooks';
+import { isEmpty } from 'lodash';
+import { withAnimationAdvanceScript, withMouseMoveEffectScript } from 'gutenverse-core/hoc';
+import { useAnimationFrontend } from 'gutenverse-core/hooks';
+import { useDisplayFrontend } from 'gutenverse-core/hooks';
+import { useAnimationAdvanceData } from 'gutenverse-core/hooks';
+
+const save = compose(
+    withAnimationAdvanceScript('icon'),
+    withMouseMoveEffectScript
+)((props) => {
+    const {
+        attributes
+    } = props;
+
+    const {
+        elementId,
+        icon,
+        url,
+        ariaLabel,
+        linkTarget,
+        rel,
+        iconShape,
+        iconView,
+    } = attributes;
+
+    const advanceAnimationData = useAnimationAdvanceData(attributes);
+    const animationClass = useAnimationFrontend(attributes);
+    const displayClass = useDisplayFrontend(attributes);
+
+    const className = classnames(
+        'guten-element',
+        elementId,
+        'guten-icon',
+        animationClass,
+        displayClass,
+    );
+
+    const wrapperClass = classnames(
+        'guten-icon-wrapper',
+        iconShape,
+        iconView,
+    );
+
+    const IconELement = () => {
+        const href = applyFilters(
+            'gutenverse.dynamic.generate-url',
+            url,
+            'dynamicUrl',
+            attributes,
+            elementId
+        );
+
+        const iconElement = !isEmpty(url) ?
+            <a className={wrapperClass} href={href} target={ linkTarget } rel={ rel } aria-label={ariaLabel}>
+                <i className={`${icon}`}/>
+            </a> : <a className={wrapperClass} href={'#'}>
+                <i className={`${icon}`}/>
+            </a>;
+
+        return iconElement;
+    };
+    return <div {...useBlockProps.save({ className, ...advanceAnimationData })}>
+        <IconELement />
+    </div>;
+});
+
+export default save;
