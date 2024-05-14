55be0e3ab63790f3563be60ad47fec4b78e44ee7
Yoga <yoga@jegstudio.com>
2024-05-14 10:01:42 +0800

    [main] remove console and error logs

--- gutenverse/includes/style/class-post-block.php
+++ gutenverse/includes/style/class-post-block.php
@@ -91,8 +91,7 @@ class Post_Block extends Style_Abstract {
 			);
 		}
 
-		if ( isset( $this->attrs['readmoreEnabled']) || isset( $this->attrs['commentEnabled'] ) ) {
-			gutenverse_jlog( 'masuk' );
+		if ( isset( $this->attrs['readmoreEnabled'] ) || isset( $this->attrs['commentEnabled'] ) ) {
 			$this->inject_style(
 				array(
 					'selector'       => ".{$this->element_id} .guten-block-container .guten-postblock-content .guten-post-meta-bottom",
