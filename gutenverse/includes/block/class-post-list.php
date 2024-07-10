02c2574c3773859fa79d6413cb581e9c6261fedd
Yoga <yoga@jegstudio.com>
2024-07-10 10:18:41 +0800

    [main] fix issue escaping data

--- gutenverse/includes/block/class-post-list.php
+++ gutenverse/includes/block/class-post-list.php
@@ -140,13 +140,13 @@ class Post_List extends Post_Abstract {
 				$date      = esc_attr( $this->format_date( $post ) );
 
 				if ( $date_icon ) {
-					$meta_date = '<i aria-hidden="true" class="' . $date_icon . '"></i> &nbsp;' . $date;
+					$meta_date = '<i aria-hidden="true" class="' . esc_attr( $date_icon ) . '"></i> &nbsp;' . $date;
 					if ( isset( $this->attributes['metaDateIconPosition'] ) ) {
 						$icon_position = esc_attr( $this->attributes['metaDateIconPosition'] );
 						if ( 'before' === $icon_position ) {
-							$meta_date = '<div class="guten-meta-date icon-position-' . $icon_position . '"><i aria-hidden="true" class="' . $date_icon . '"></i> &nbsp;' . $date . '</div>';
+							$meta_date = '<div class="guten-meta-date icon-position-' . $icon_position . '"><i aria-hidden="true" class="' . esc_attr( $date_icon ) . '"></i> &nbsp;' . $date . '</div>';
 						} else {
-							$meta_date = '<div class="guten-meta-date icon-position-' . $icon_position . '">' . $date . '&nbsp;<i aria-hidden="true" class="' . $date_icon . '"></i></div>';
+							$meta_date = '<div class="guten-meta-date icon-position-' . $icon_position . '">' . $date . '&nbsp;<i aria-hidden="true" class="' . esc_attr( $date_icon ) . '"></i></div>';
 						}
 					}
 				}
@@ -159,7 +159,7 @@ class Post_List extends Post_Abstract {
 				$category      = get_category( $this->get_primary_category( $post->ID ) );
 
 				if ( $category_icon && isset( $category->name ) ) {
-					$meta_category = '<i aria-hidden="true" class="' . $category_icon . '"></i> ' . $category->name;
+					$meta_category = '<i aria-hidden="true" class="' . esc_attr( $category_icon ) . '"></i> ' . $category->name;
 				}
 
 				$meta_category = '<span class="meta-category">' . $meta_category . '</span>';
