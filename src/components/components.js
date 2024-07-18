3441f5699953f59516266c8b0afdcac08e87195a
Hendra <nndmhndr@gmail.com>
2024-07-18 16:24:11 +0800

    update add on post change

--- src/components/components.js
+++ src/components/components.js
@@ -4,6 +4,8 @@ import domReady from '@wordpress/dom-ready';
 import GutenverseLibrary from './library';
 import elementChange from 'element-change';
 import { applyFilters } from '@wordpress/hooks';
+import { withAutoRecovery } from 'gutenverse-core/hoc';
+import { select, subscribe } from '@wordpress/data';
 
 /* Other Editor Components */
 import './hook';
@@ -16,7 +18,18 @@ import './copy-style/paste-style';
 elementChange('#site-editor', () => {
     const library = document.getElementById('gutenverse-library-button');
     // const globalStyle = document.getElementById('global-style-options');
+    const editorStore = 'core/editor';
 
+    let previousContent = select(editorStore).getEditedPostContent();
+
+    subscribe(() => {
+        const currentContent = select(editorStore).getEditedPostContent();
+
+        if (currentContent !== previousContent) {
+            previousContent = currentContent;
+            withAutoRecovery(null);
+        }
+    });
     if (library === null) {
         const content = applyFilters(
             'gutenverse.site.editor.content',
