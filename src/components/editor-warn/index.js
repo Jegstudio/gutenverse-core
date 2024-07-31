6ce7015e7cff1358793d47eeea14d6a8c50c8dcc
Hendra <nndmhndr@gmail.com>
2024-07-31 16:09:42 +0800

    add plugin install instead get plugin data from dashboard ecosystem

--- src/components/editor-warn/index.js
+++ src/components/editor-warn/index.js
@@ -1,9 +1,15 @@
-import { select, subscribe } from '@wordpress/data';
+import { select, subscribe, dispatch } from '@wordpress/data';
 import { render } from '@wordpress/element';
-import { useState } from '@wordpress/element';
+import { useState, useEffect } from '@wordpress/element';
 import apiFetch from '@wordpress/api-fetch';
 import { __ } from '@wordpress/i18n';
 import debounce from 'lodash/debounce';
+import { fetchLibraryData } from 'gutenverse-core/requests';
+import classnames from 'classnames';
+import isEmpty from 'lodash/isEmpty';
+import semver from 'semver';
+import { Loader } from 'react-feather';
+import { LogoIconGutenverseSVG } from 'gutenverse-core/icons';
 
 export const editorWarn = () => {
     const checkMissingBlock = debounce(() => {
@@ -62,9 +68,41 @@ const WarnModal = (props) => {
     const { missingBlock } = props;
     const [open, setOpen] = useState(true);
     const [doNotShowAgain, setDoNotShowAgain] = useState(false);
+    const [pluginsEco, setPluginsEco] = useState(false);
+    const [missingPlugin, setMissingPlugins] = useState(false);
+    const [plugins, setPlugins] = useState(window['GutenverseConfig']?.plugins);
 
     const doNotShow = () => {
         setDoNotShowAgain(!doNotShowAgain);
+    };
+
+    const activatePlugin = (pluginKey) => {
+        setPlugins((prevPlugins) => {
+            if (prevPlugins[pluginKey]) {
+                return {
+                    ...prevPlugins,
+                    [pluginKey]: {
+                        ...prevPlugins[pluginKey],
+                        active: true,
+                    },
+                };
+            }
+            return prevPlugins;
+        });
+    };
+
+    const installPlugin = ({ slug, name, path, version }) => {
+        setPlugins((prevPlugins) => {
+            return {
+                ...prevPlugins,
+                [slug]: {
+                    active: true,
+                    name: name,
+                    path: path,
+                    version: version,
+                },
+            };
+        });
     };
 
     const closeWarn = () => {
@@ -84,7 +122,55 @@ const WarnModal = (props) => {
         }
     };
 
-    return open ? (
+    useEffect(() => {
+        const dev = 'true' == '--dev_mode--';
+        if (!pluginsEco) {
+            const fetchData = async (dev) => {
+                const result = await fetchLibraryData(dev);
+                setPluginsEco(result?.['plugin-ecosystem']);
+            };
+
+            if (dev) {
+                fetchData(true);
+            } else {
+                fetchData(false);
+            }
+        } else {
+            const slugsToKeep = new Set();
+            const defaultSlug = 'gutenverse';
+
+            missingBlock.forEach(type => {
+                const parts = type.split('/');
+                const prefix = parts[0];
+                const suffix = parts[1].split('-')[0];
+                const slug = `${prefix}-${suffix}`;
+                slugsToKeep.add(slug);
+            });
+
+            let filteredPlugins = pluginsEco.filter(plugin => slugsToKeep.has(plugin.slug));
+
+            const matchedSlugs = new Set(filteredPlugins.map(plugin => plugin.slug));
+            const unmatched = [...slugsToKeep].some(slug => !matchedSlugs.has(slug));
+
+            if (unmatched) {
+                const defaultEntry = pluginsEco.find(plugin => plugin.slug === defaultSlug);
+                if (defaultEntry) {
+                    filteredPlugins.push(defaultEntry);
+                }
+            }
+
+            const pluginsData = {
+                plugins: plugins,
+                pluginEcosystem: filteredPlugins,
+                installPlugin,
+                activatePlugin,
+            };
+
+            setMissingPlugins(pluginsData);
+        }
+    }, [open, pluginsEco, plugins]);
+
+    return open && missingPlugin && missingPlugin.pluginEcosystem.length ? (
         <div className="gutenverse-editor-warn">
             <div className="gutenverse-warn-wrapper">
                 <div className="gutenverse-warn-header">
@@ -92,13 +178,16 @@ const WarnModal = (props) => {
                     <span>{__('Missing Gutenverse Block', '--gctd--')}</span>
                 </div>
                 <div className="gutenverse-warn-description">
-                    {__('Some blocks are missing. They might be disabled or require the corresponding Gutenverse plugins to be installed and activated to restore these blocks:', '--gctd--')}
+                    {__('Some blocks are missing. They might be disabled or require the corresponding Gutenverse plugins to be installed and activated to restore the blocks:', '--gctd--')}
                 </div>
-                <ul className="gutenverse-warn-list">
+                {/* <ul className="gutenverse-warn-list">
                     {missingBlock.map((block, index) => (
                         <li key={index} style={{ marginBottom: '5px' }}>{block}</li>
                     ))}
-                </ul>
+                </ul> */}
+                <div className="gutenverse-warn-plugin-list">
+                    <PluginsData {...missingPlugin} />
+                </div>
                 <div className="gutenverse-warn-footer">
                     <div className="warn-checkbox" onClick={doNotShow}>
                         {doNotShowAgain ? <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -120,4 +209,195 @@ const WarnModal = (props) => {
             </div>
         </div>
     ) : null;
+};
+
+const PluginItem = ({
+    plugin,
+    plugins,
+    ...props
+}) => {
+    const { slug, name, version, icons, description, incoming, icon, url, host } = plugin;
+    const [loadingString, setLoadingString] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [isRetry, setIsRetry] = useState(true);
+    const installed = plugins[slug];
+    let action = null;
+    const downloadZip = (url) => {
+        window.location.href = url;
+    };
+    const installingPlugin = () => {
+        setLoading(true);
+        setLoadingString(__('Installing Plugin', '--gctd--'));
+
+        apiFetch({
+            path: 'wp/v2/plugins',
+            method: 'POST',
+            data: {
+                slug,
+                status: 'active'
+            },
+        }).then(res => {
+            const { version, name, plugin } = res;
+            props.installPlugin({
+                slug: slug,
+                name: name,
+                path: plugin,
+                version: version
+            });
+            setLoading(false);
+        }).then(() => { }).catch(() => {
+            alert('Error during installing plugin');
+        }).finally(() => {
+            setLoading(false);
+            setLoadingString('');
+        });
+    };
+
+    const updatingPlugin = () => {
+        setLoading(true);
+        setLoadingString(__('Disabling Plugin', '--gctd--'));
+
+        if (installed) {
+            apiFetch({
+                path: `wp/v2/plugins/plugin?plugin=${installed?.path}`,
+                method: 'POST',
+                data: {
+                    status: 'inactive'
+                }
+            }).then(() => {
+                setLoadingString(__('Deleting Plugin', '--gctd--'));
+
+                return apiFetch({
+                    path: `wp/v2/plugins/plugin?plugin=${installed?.path}`,
+                    method: 'DELETE',
+                });
+            }).then(() => {
+                installingPlugin();
+            });
+        }
+    };
+
+    const activatingPlugin = (update) => {
+        if (update) {
+            updatingPlugin();
+        } else {
+            setLoading(true);
+            setLoadingString(__('Activating Plugin', '--gctd--'));
+            apiFetch({
+                path: `wp/v2/plugins/plugin?plugin=${installed?.path}`,
+                method: 'POST',
+                data: {
+                    status: 'active'
+                }
+            }).then(res => {
+                const { plugin } = res;
+                const slug = plugin.split('/');
+                props.activatePlugin(slug[0]);
+                setLoading(false);
+                setLoadingString('');
+            }).catch(() => {
+                if (isRetry) {
+                    setIsRetry(false);
+                    activatingPlugin(false);
+                }
+            });
+        }
+    };
+
+    const singleClass = classnames('install-action', {
+        'loading': loading,
+    });
+
+    const loadingCircle = loading && <div className="rotating" style={{ display: 'flex' }}>
+        <Loader size={20} />
+    </div>;
+
+    if (installed) {
+        const invalidVersion = !isEmpty(version) && !semver.gte(installed.version, version || '0.0.0');
+        const string = invalidVersion ? __('Update & Activate Plugin', '--gctd--') : __('Activate Plugin', '--gctd--');
+        if (installed.active === false) {
+            action = <div className={`${singleClass} update`} onClick={() => {
+                activatingPlugin(invalidVersion);
+                setIsRetry(true);
+            }}>
+                {loadingCircle}
+                {loading ? loadingString : string}
+            </div>;
+        } else {
+            if (invalidVersion) {
+                action = <div className={`${singleClass} update`} onClick={() => host === 'server' ? downloadZip(url) : updatingPlugin()}>
+                    {loadingCircle}
+                    {loading ? loadingString : __('Update Plugin', '--gctd--')}
+                </div>;
+            }
+
+            action = <div className={`${singleClass} installed`} >
+                {loadingCircle}
+                {loading ? loadingString : __('Done', '--gctd--')}
+            </div>;
+        }
+    } else if (!installed && incoming == 0) {
+        action = <div className={singleClass} onClick={() => host === 'server' ? downloadZip(url) : installingPlugin()}>
+            {loadingCircle}
+            {loading ? loadingString : __('Download Plugin', '--gctd--')}
+        </div>;
+    } else {
+        action = <div className={`${singleClass} installed`}>
+            {loadingCircle}
+            {loading ? loadingString : __('Release Soon', '--gctd--')}
+        </div>;
+    }
+    return <div key={slug} className="plugin-item">
+        {
+            incoming === '1' && <div className="ribbon">SOON</div>
+        }
+        {
+            icon ? <div className="icon-wrapper">
+                <img className="icon-plugin" src={icon[0]} /> </div>
+                : icons ?
+                    <div className="icon-wrapper">
+                        <img className="icon-plugin" src={icons}></img></div>
+                    :
+                    <div className="icon-wrapper">
+                        <LogoIconGutenverseSVG className="icon-plugin" /></div>
+
+        }
+        <div className="details">
+
+            <h2 className="plugin-title">
+                {name.includes('Gutenverse') ? <>
+                    <span>{__('Gutenverse', '--gctd--')}</span>&nbsp;
+                    {name.split('Gutenverse').join('')}
+                </> : name}
+            </h2>
+            {version ?
+                <p className="plugin-version">
+                    {__('Version ', '--gctd--')}
+                    {version}
+                </p> :
+                <p className="plugin-version">
+                    {__('Coming Soon', '--gctd--')}
+                </p>
+            }
+            {/* {description && <p className="plugin-description">
+                {description.substring(0, 80) + '...'}
+            </p>} */}
+        </div>
+        <div className="plugin-actions">
+            {action}
+        </div>
+    </div>;
+};
+
+const PluginsData = ({
+    pluginEcosystem,
+    ...props
+}) => {
+    return pluginEcosystem && pluginEcosystem.map((plugin, i) => {
+        return <PluginItem
+            key={i}
+            plugin={plugin}
+            {...props}
+        />;
+    });
 };
