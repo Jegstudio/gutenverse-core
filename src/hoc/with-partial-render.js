87e8dc3d6beab9058b8aac5fe6e2e26b69e46d02
Yoga <yoga@jegstudio.com>
2024-11-19 13:48:01 +0800

    fix issue with loading in post editor and template/pattern preview

--- src/hoc/with-partial-render.js
+++ src/hoc/with-partial-render.js
@@ -1,4 +1,6 @@
 import { useRef, useState, useEffect } from '@wordpress/element';
+import { Skeleton } from 'gutenverse-core/components';
+// import { getDeviceType } from 'gutenverse-core/editor-helper';
 import { isOnEditor } from 'gutenverse-core/helper';
 
 const BlockLoading = ({
@@ -6,30 +8,57 @@ const BlockLoading = ({
 }) => {
     return <div className="gutenverse-block-loading" ref={renderRef}>
         <div></div>
+        <Skeleton width="100%" height="200px" />
     </div>;
 };
 
 export const withPartialRender = (BlockElement) => {
     return (props) => {
         const [partialRender, setPartialRender] = useState(false);
-        const [windowElement, setWindowElement] = useState(null);
+        const [windowData, setWindowData] = useState({});
         const renderRef = useRef();
+        // const deviceType = getDeviceType();
 
         useEffect(() => {
             startPartialRender();
-        }, [renderRef, windowElement]);
+        }, [renderRef, windowData]);
 
         useEffect(() => {
-            if (renderRef?.current && renderRef?.current?.ownerDocument) {
-                const windowEl = renderRef?.current?.ownerDocument?.defaultView || renderRef?.current?.ownerDocument?.parentWindow;
-                setWindowElement(windowEl);
-            }
+            const timeout = setTimeout(() => {
+                if (!renderRef?.current || !renderRef?.current?.ownerDocument) {
+                    return;
+                }
+
+                // check if this is site editor
+                if (window.location.href.indexOf('site-editor.php') !== -1) {
+                    const windowEl = renderRef?.current?.ownerDocument?.defaultView || renderRef?.current?.ownerDocument?.parentWindow;
+                    setWindowData({
+                        type: 'site',
+                        window: windowEl
+                    });
+                } else {
+                    const scrollEl = document?.getElementsByClassName('interface-interface-skeleton__content');
+                    scrollEl?.length > 0 && setWindowData({
+                        type: 'post',
+                        window: scrollEl[0]
+                    });
+                }
+            }, 250);
+
+            return () => clearTimeout(timeout);
         }, [renderRef]);
 
+        // useEffect(() => {
+        //     if (partialRender) {
+        //         setPartialRender(false);
+        //     }
+        // }, [deviceType]);
+
         const startPartialRender = () => {
-            if ( windowElement && renderRef?.current && renderRef?.current?.getBoundingClientRect ) {
-                const positionData = renderRef?.current?.getBoundingClientRect();
-                const checking = isOnEditor() ? (positionData?.top < (3 * windowElement?.innerHeight)) : (positionData?.top < (2 * windowElement?.innerHeight));
+            if ( !partialRender && windowData?.type && renderRef?.current && renderRef?.current?.getBoundingClientRect ) {
+                const elementPosition = renderRef?.current?.getBoundingClientRect();
+                const windowHeight = windowData?.type === 'post' ? windowData?.window?.getBoundingClientRect()?.height : windowData?.window?.innerHeight;
+                const checking = isOnEditor() ? (elementPosition?.top < (3 * windowHeight)) : (elementPosition?.top < (1.5 * windowHeight));
 
                 if (checking) {
                     setPartialRender(true);
@@ -38,7 +67,7 @@ export const withPartialRender = (BlockElement) => {
         };
 
         // Move this to useEffect
-        windowElement?.addEventListener('scroll', () => {
+        windowData?.window?.addEventListener('scroll', () => {
             startPartialRender();
         });
 
