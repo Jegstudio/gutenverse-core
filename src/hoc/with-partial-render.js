fa87b0f502d925ea7c1d3d96faa2c748c9184bd0
Yoga <yoga@jegstudio.com>
2024-11-20 12:12:29 +0800

    update intersection parameter

--- src/hoc/with-partial-render.js
+++ src/hoc/with-partial-render.js
@@ -1,5 +1,6 @@
 // import { useSelect } from '@wordpress/data';
 import { useRef, useState, useEffect } from '@wordpress/element';
+import { isOnEditor } from 'gutenverse-core/helper';
 // import { Skeleton } from 'gutenverse-core/components';
 // import { getDeviceType } from 'gutenverse-core/editor-helper';
 // import { isOnEditor } from 'gutenverse-core/helper';
@@ -15,7 +16,6 @@ const BlockLoading = ({
 export const withPartialRender = (BlockElement) => {
     return (props) => {
         const [partialRender, setPartialRender] = useState(false);
-        const [windowData, setWindowData] = useState({});
         const renderRef = useRef();
         // const deviceType = getDeviceType();
 
@@ -25,6 +25,7 @@ export const withPartialRender = (BlockElement) => {
             }
 
             const blockElement = renderRef.current;
+            const windowEl = renderRef.current.ownerDocument;
 
             const observer = new IntersectionObserver(
                 (entries) => {
@@ -36,8 +37,9 @@ export const withPartialRender = (BlockElement) => {
                     });
                 },
                 {
-                    rootMargin: '240px',
-                    threshold: 0.01,
+                    root: windowEl,
+                    rootMargin: isOnEditor() ? '150% 0px' : '0px',
+                    threshold: 0,
                 }
             );
 
@@ -48,58 +50,12 @@ export const withPartialRender = (BlockElement) => {
             };
         }, [renderRef]);
 
-        // useEffect(() => {
-        //     startPartialRender();
-        // }, [renderRef, windowData]);
-
-        // useEffect(() => {
-        //     const timeout = setTimeout(() => {
-        //         if (!renderRef?.current || !renderRef?.current?.ownerDocument) {
-        //             return;
-        //         }
-
-        //         const elDocument = renderRef?.current?.ownerDocument;
-        //         const postEditor = elDocument?.getElementsByClassName('interface-interface-skeleton__content');
-
-        //         if (postEditor?.length > 0) {
-        //             setWindowData({
-        //                 type: 'post',
-        //                 window: postEditor[0]
-        //             });
-        //         } else {
-        //             const iframeEditor = elDocument?.defaultView || elDocument?.parentWindow;
-        //             setWindowData({
-        //                 type: 'site',
-        //                 window: iframeEditor
-        //             });
-        //         }
-        //     }, 500);
-
-        //     return () => clearTimeout(timeout);
-        // }, [blocks]);
 
         // // useEffect(() => {
         // //     if (partialRender) {
         // //         setPartialRender(false);
         // //     }
         // // }, [deviceType]);
-
-        // const startPartialRender = () => {
-        //     if ( !partialRender && windowData?.type && renderRef?.current && renderRef?.current?.getBoundingClientRect ) {
-        //         const elementPosition = renderRef?.current?.getBoundingClientRect();
-        //         const windowHeight = windowData?.type === 'post' ? windowData?.window?.getBoundingClientRect()?.height : windowData?.window?.innerHeight;
-        //         const checking = isOnEditor() ? (elementPosition?.top < (3 * windowHeight)) : (elementPosition?.top < (1.5 * windowHeight));
-
-        //         if (checking) {
-        //             setPartialRender(true);
-        //         }
-        //     }
-        // };
-
-        // // Move this to useEffect
-        // windowData?.window?.addEventListener('scroll', () => {
-        //     startPartialRender();
-        // });
 
         return partialRender ? <BlockElement {...props}/> : <BlockLoading renderRef={renderRef}/>;
     };
