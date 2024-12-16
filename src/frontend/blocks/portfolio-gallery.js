1b1c57d166d7493cb3c9388c3b038c7cb3d75804
Arya1Putra <putra1arya@gmail.com>
2024-12-16 15:10:24 +0800

    [dev/portfolio-gallery] portfolio gallery frontend done

---
+++ src/frontend/blocks/portfolio-gallery.js
@@ -0,0 +1,56 @@
+import { Default, u } from 'gutenverse-core-frontend';
+
+class GutenversePortfolioGallery extends Default {
+    /* public */
+    init() {
+        this._elements.map(element => {
+            this.__handleAction(element);
+        });
+    }
+
+    /* private */
+    __handleAction(element){
+        const blockElement = u(element);
+        const behavior = blockElement.data('behavior');
+        const rowItems = blockElement.find('.row-item');
+        switch (behavior) {
+            case 'onclick':
+                this.__handleBehavior('click',rowItems, blockElement);
+                break;
+            case 'onhover':
+            default:
+                this.__handleBehavior('mouseover',rowItems, blockElement);
+                break;
+        }
+    }
+    __handleBehavior(behavior,rows, blockElement){
+        rows.on(behavior, (e) => {
+            this.__removeClass(rows, blockElement);
+            this.__addClass(e.currentTarget, blockElement);
+        });
+    }
+    __removeClass(rows, blockElement){
+        rows.each(el => {
+            const current = u(el);
+            if(current.hasClass('current-item')){
+                current.removeClass('current-item');
+                const image = current.data('tab');
+                const imageEl = blockElement.find(`#${image}`);
+                if(imageEl && imageEl.hasClass('current-item')){
+                    imageEl.removeClass('current-item');
+                }
+            }
+        })
+    }
+    __addClass(target, blockElement){
+        const current = u(target);
+        current.addClass('current-item');
+        const image = current.data('tab');
+        const imageEl = blockElement.find(`#${image}`);
+        if(imageEl && !imageEl.hasClass('current-item')){
+            imageEl.addClass('current-item');
+        }
+    }
+}
+
+export default GutenversePortfolioGallery;
