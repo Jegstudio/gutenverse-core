bfa924e12c2685a2df3ffb05580257e01859def4
Gowinda Kadek <gowinda@jegstudio.com>
2025-01-07 17:09:34 +0800

    remove opacity from the container and only apply it to the color

--- src/styling/styling/handler/handle-background-effect.js
+++ src/styling/styling/handler/handle-background-effect.js
@@ -1,4 +1,4 @@
-import { elementVar, normalAppender, responsiveAppender, DeviceLoop, deviceStyleValue } from '../styling-utility';
+import { elementVar, normalAppender } from '../styling-utility';
 import { handleBoxShadow } from 'gutenverse-core/styling';
 
 export const handleBackgroundEffect = (style) => {
@@ -6,25 +6,11 @@ export const handleBackgroundEffect = (style) => {
 
     const {
         hiddenOverflow,
-        opacity
     } = style;
     if ( hiddenOverflow ) {
         normalAppender({
             style: `overflow: ${hiddenOverflow ? 'hidden' : 'visible'};`,
             elementStyle
-        });
-    }
-    if ( opacity ) {
-        DeviceLoop(device => {
-            const _opacity = deviceStyleValue(device, opacity);
-
-            if (_opacity) {
-                responsiveAppender({
-                    style: `opacity: ${_opacity};`,
-                    device,
-                    elementStyle: elementStyle
-                });
-            }
         });
     }
     return elementStyle;
