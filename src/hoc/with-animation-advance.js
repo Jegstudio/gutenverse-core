3be0a1ac56a90f11b7b14595eeafd9efa91b62ac
Arya1Putra <putra1arya@gmail.com>
2025-01-24 15:34:14 +0800

    hoc update

--- src/hoc/with-animation-advance.js
+++ src/hoc/with-animation-advance.js
@@ -1,11 +1,17 @@
 import { applyFilters } from '@wordpress/hooks';
+import isEmpty from 'lodash/isEmpty';
 
 export const withAnimationAdvance = (blockType) => (BlockElement) => {
     return (props) => {
-        return applyFilters(
-            'gutenverse.hoc.advance-animation',
-            <BlockElement {...props} />,
-            { BlockElement, blockType, props }
-        );
+        const {attributes} = props;
+        if(!isEmpty(attributes.advanceAnimation)){
+            return applyFilters(
+                'gutenverse.hoc.advance-animation',
+                <BlockElement {...props} />,
+                { BlockElement, blockType, props }
+            );
+        }else{
+            return <BlockElement {...props} />
+        }
     };
 };
--- src/hoc/with-animation-background.js
+++ src/hoc/with-animation-background.js
@@ -1,11 +1,17 @@
 import { applyFilters } from '@wordpress/hooks';
+import isEmpty from 'lodash/isEmpty';
 
 export const withAnimationBackground = (blockType) => (BlockElement) => {
     return (props) => {
-        return applyFilters(
-            'gutenverse.hoc.background-animation',
-            <BlockElement {...props} />,
-            { BlockElement, blockType, props }
-        );
+        const {attributes} = props;
+        if(!isEmpty(attributes.backgroundAnimated)){
+            return applyFilters(
+                'gutenverse.hoc.background-animation',
+                <BlockElement {...props} />,
+                { BlockElement, blockType, props }
+            );
+        }else{
+            return <BlockElement {...props} />
+        }
     };
 };
