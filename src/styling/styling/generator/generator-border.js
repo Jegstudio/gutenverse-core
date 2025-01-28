d925f5b9bbc3e06d713ff88b988015e2e5bf158e
Yoga <yoga@jegstudio.com>
2025-01-28 15:35:02 +0800

    update border styling

--- src/styling/styling/generator/generator-border.js
+++ src/styling/styling/generator/generator-border.js
@@ -15,17 +15,15 @@ export const borderCSS = (value) => {
         ...keys.filter(key => key !== 'all'),
     ];
 
+    /**
+     * This is an old way of styling border.
+     * Styling only applied on Desktop, because in newer version the attribute `borderResponsive` is used for Tablet and Mobile.
+     */
     sortedKeys.forEach((pos) => {
         if (pos === 'radius') {
             const radius = value[pos];
             if (radius?.Desktop) {
                 border.Desktop.push(`${handleDimension(radius.Desktop, 'border-radius', false)}`);
-            }
-            if (radius?.Tablet) {
-                border.Tablet.push(`${handleDimension(radius.Tablet, 'border-radius', false)}`);
-            }
-            if (radius?.Mobile) {
-                border.Mobile.push(`${handleDimension(radius.Mobile, 'border-radius', false)}`);
             }
         } else if (!isEmpty(value[pos]) && value[pos].type && value[pos].type !== 'default') {
             const borderAttr = value[pos];
@@ -33,21 +31,15 @@ export const borderCSS = (value) => {
 
             if (borderAttr.type) {
                 border.Desktop.push(`border-${posPrefix}style: ${borderAttr.type};`);
-                border.Tablet.push(`border-${posPrefix}style: ${borderAttr.type};`);
-                border.Mobile.push(`border-${posPrefix}style: ${borderAttr.type};`);
             }
 
             if (borderAttr.width) {
                 border.Desktop.push(`border-${posPrefix}width: ${borderAttr.width}px;`);
-                border.Tablet.push(`border-${posPrefix}width: ${borderAttr.width}px;`);
-                border.Mobile.push(`border-${posPrefix}width: ${borderAttr.width}px;`);
             }
 
             if (borderAttr.color) {
                 const colorCSS = handleColor(borderAttr.color, `border-${posPrefix}color`);
                 border.Desktop.push(colorCSS);
-                border.Tablet.push(colorCSS);
-                border.Mobile.push(colorCSS);
             }
         }
     });
