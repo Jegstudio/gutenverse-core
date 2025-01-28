2f9b3eb8380c15373c359a70a94568462878e173
Yoga <yoga@jegstudio.com>
2025-01-28 15:30:13 +0800

    [dev/improve-editor-styling] update styling border and background

--- src/editor/blocks/heading/styles/block.js
+++ src/editor/blocks/heading/styles/block.js
@@ -18,6 +18,36 @@ const getBlockStyle = (elementId) => {
             'id': 'typography',
             'selector': `h1.guten-element.${elementId},h2.guten-element.${elementId},h3.guten-element.${elementId},h4.guten-element.${elementId},h5.guten-element.${elementId},h6.guten-element.${elementId}`,
         },
+        {
+            'type': 'background',
+            'id': 'background',
+            'selector': `h1.guten-element.${elementId},h2.guten-element.${elementId},h3.guten-element.${elementId},h4.guten-element.${elementId},h5.guten-element.${elementId},h6.guten-element.${elementId}`,
+        },
+        {
+            'type': 'background',
+            'id': 'backgroundHover',
+            'selector': `h1.guten-element.${elementId}:hover,h2.guten-element.${elementId}:hover,h3.guten-element.${elementId}:hover,h4.guten-element.${elementId}:hover,h5.guten-element.${elementId}:hover,h6.guten-element.${elementId}:hover`,
+        },
+        {
+            'type': 'border',
+            'id': 'border',
+            'selector': `h1.guten-element.${elementId},h2.guten-element.${elementId},h3.guten-element.${elementId},h4.guten-element.${elementId},h5.guten-element.${elementId},h6.guten-element.${elementId}`,
+        },
+        {
+            'type': 'border',
+            'id': 'borderHover',
+            'selector': `h1.guten-element.${elementId}:hover,h2.guten-element.${elementId}:hover,h3.guten-element.${elementId}:hover,h4.guten-element.${elementId}:hover,h5.guten-element.${elementId}:hover,h6.guten-element.${elementId}:hover`,
+        },
+        {
+            'type': 'borderResponsive',
+            'id': 'borderResponsive',
+            'selector': `h1.guten-element.${elementId},h2.guten-element.${elementId},h3.guten-element.${elementId},h4.guten-element.${elementId},h5.guten-element.${elementId},h6.guten-element.${elementId}`,
+        },
+        {
+            'type': 'borderResponsive',
+            'id': 'borderResponsiveHover',
+            'selector': `h1.guten-element.${elementId}:hover,h2.guten-element.${elementId}:hover,h3.guten-element.${elementId}:hover,h4.guten-element.${elementId}:hover,h5.guten-element.${elementId}:hover,h6.guten-element.${elementId}:hover`,
+        },
     ];
 };
 
