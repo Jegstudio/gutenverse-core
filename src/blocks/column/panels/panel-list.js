57cd5a200ca287ce2fb5482ac5046507d5c26c6d
Yoga <yoga@jegstudio.com>
2025-01-31 10:06:40 +0800

    temporary disable section & column panel list

--- src/blocks/column/panels/panel-list.js
+++ src/blocks/column/panels/panel-list.js
@@ -7,150 +7,150 @@ import { blurPanel } from './panel-blur';
 
 export const panelList = () => {
     return [
-        {
-            title: __('Layouts', '--gctd--'),
-            panelArray: layoutPanel,
-            tabRole: TabSetting
-        },
-        {
-            title: __('Sticky', '--gctd--'),
-            panelArray: stickyPanel,
-            pro: true,
-        },
-        {
-            title: __('Cursor Effect', '--gctd--'),
-            initialOpen: false,
-            panelArray: cursorEffectPanel,
-            tabRole: TabSetting,
-            pro: true,
-        },
-        {
-            title: __('Background Effect', '--gctd--'),
-            initialOpen: false,
-            panelArray: (props) =>  backgroundEffectPanel({
-                ...props,
-                selector: `.${props.elementId}> .guten-column-resizeable> .sticky-wrapper> .guten-column-wrapper > .guten-background-effect`
-            }),
-            tabRole: TabSetting,
-            pro: true,
-        },
-        {
-            title: __('Background', '--gctd--'),
-            initialOpen: false,
-            panelArray: (props) => backgroundPanel({
-                ...props,
-                blockType: 'column',
-                styleId: 'column-background',
-                normalOptions: ['default', 'gradient', 'fluid', 'slide'],
-                hoverOptions: ['default', 'gradient'],
-                normalSelector: `.editor-styles-wrapper .is-root-container .${props.elementId}:not(.background-animated) > .guten-column-resizeable > .sticky-wrapper > .guten-column-wrapper, .${props.elementId}.background-animated > .guten-column-resizeable > .sticky-wrapper > .guten-column-wrapper > .guten-background-animated .animated-layer`,
-                hoverSelector: `.editor-styles-wrapper .is-root-container .${props.elementId}:not(.background-animated) > .guten-column-resizeable > .sticky-wrapper > .guten-column-wrapper:hover, .${props.elementId}.background-animated > .guten-column-resizeable > .sticky-wrapper > .guten-column-wrapper:hover > .guten-background-animated .animated-layer`
-            }),
-            tabRole: TabStyle
-        },
-        {
-            title: __('Background Overlay', '--gctd--'),
-            initialOpen: false,
-            panelArray: (props) => backgroundOverlayPanel({
-                ...props,
-                styleId: 'column-background-overlay',
-                normalOptions: ['default', 'gradient'],
-                hoverOptions: ['default', 'gradient'],
-                normalSelector: ` .${props.elementId} > .guten-column-resizeable > .sticky-wrapper > .guten-column-wrapper > .guten-background-overlay, .${props.elementId}.background-animated > .guten-column-resizeable > .sticky-wrapper > .guten-column-wrapper > .guten-background-overlay`,
-                hoverSelector: `.${props.elementId} > .guten-column-resizeable > .sticky-wrapper > .guten-column-wrapper:hover > .guten-background-overlay, .${props.elementId}.background-animated > .guten-column-resizeable > .sticky-wrapper > .guten-column-wrapper:hover > .guten-background-overlay`
-            }),
-            tabRole: TabStyle
-        },
-        {
-            title: __('Blur', '--gctd--'),
-            initialOpen: false,
-            panelArray: blurPanel,
-            tabRole: TabStyle
-        },
-        {
-            title: __('Border', '--gctd--'),
-            initialOpen: false,
-            panelArray: (props) => borderPanel({
-                ...props,
-                styleId: 'column-border',
-                selector: `.editor-styles-wrapper .is-root-container .${props.elementId} > .guten-column-resizeable > .sticky-wrapper > .guten-column-wrapper`,
-            }),
-            tabRole: TabStyle
-        },
-        {
-            title: __('Masking', '--gctd--'),
-            initialOpen: false,
-            panelArray: maskPanel,
-            tabRole: TabStyle
-        },
-        {
-            title: __('Pointer Event', '--gctd--'),
-            initialOpen: false,
-            panelArray: pointerEventPanel,
-            tabRole: TabStyle
-        },
-        {
-            title: __('Mouse Move Effect', 'gutenverse'),
-            initialOpen: false,
-            panelArray: mouseMoveEffectPanel,
-            tabRole: TabSetting,
-            pro: true,
-        },
-        {
-            title: __('Typography', '--gctd--'),
-            initialOpen: false,
-            panelArray: (props) => typographyPanel({
-                ...props,
-                styleId: 'column-typography'
-            }),
-            tabRole: TabStyle
-        },
-        {
-            title: __('Display', '--gctd--'),
-            initialOpen: false,
-            panelArray: responsivePanel,
-            tabRole: TabSetting
-        },
-        {
-            title: __('Animation Effects', '--gctd--'),
-            initialOpen: false,
-            panelArray: (props) => animationPanel({
-                ...props,
-                styleId: 'column-animation'
-            }),
-            tabRole: TabSetting
-        },
-        {
-            title: __('Background Animation', '--gctd--'),
-            initialOpen: false,
-            panelArray: backgroundAnimatedPanel,
-            pro: true,
-        },
-        {
-            title: __('Advanced Animation', '--gctd--'),
-            initialOpen: false,
-            panelAdvance: true,
-            panelArray: (props) => advanceAnimationPanel({
-                ...props,
-                blockType: 'column'
-            }),
-            pro: true,
-        },
-        {
-            title: __('Spacing', '--gctd--'),
-            initialOpen: false,
-            panelArray: (props) => advancePanel({
-                ...props,
-                styleId: 'column-advance',
-                selector: `.editor-styles-wrapper .is-root-container .${props.elementId} > .guten-column-resizeable > .sticky-wrapper > .guten-column-wrapper`
-            }),
-            tabRole: TabSetting
-        },
-        {
-            title: __('Condition', 'gutenverse'),
-            panelArray: conditionPanel,
-            initialOpen: false,
-            pro: true
-        },
+        // {
+        //     title: __('Layouts', '--gctd--'),
+        //     panelArray: layoutPanel,
+        //     tabRole: TabSetting
+        // },
+        // {
+        //     title: __('Sticky', '--gctd--'),
+        //     panelArray: stickyPanel,
+        //     pro: true,
+        // },
+        // {
+        //     title: __('Cursor Effect', '--gctd--'),
+        //     initialOpen: false,
+        //     panelArray: cursorEffectPanel,
+        //     tabRole: TabSetting,
+        //     pro: true,
+        // },
+        // {
+        //     title: __('Background Effect', '--gctd--'),
+        //     initialOpen: false,
+        //     panelArray: (props) =>  backgroundEffectPanel({
+        //         ...props,
+        //         selector: `.${props.elementId}> .guten-column-resizeable> .sticky-wrapper> .guten-column-wrapper > .guten-background-effect`
+        //     }),
+        //     tabRole: TabSetting,
+        //     pro: true,
+        // },
+        // {
+        //     title: __('Background', '--gctd--'),
+        //     initialOpen: false,
+        //     panelArray: (props) => backgroundPanel({
+        //         ...props,
+        //         blockType: 'column',
+        //         styleId: 'column-background',
+        //         normalOptions: ['default', 'gradient', 'fluid', 'slide'],
+        //         hoverOptions: ['default', 'gradient'],
+        //         normalSelector: `.editor-styles-wrapper .is-root-container .${props.elementId}:not(.background-animated) > .guten-column-resizeable > .sticky-wrapper > .guten-column-wrapper, .${props.elementId}.background-animated > .guten-column-resizeable > .sticky-wrapper > .guten-column-wrapper > .guten-background-animated .animated-layer`,
+        //         hoverSelector: `.editor-styles-wrapper .is-root-container .${props.elementId}:not(.background-animated) > .guten-column-resizeable > .sticky-wrapper > .guten-column-wrapper:hover, .${props.elementId}.background-animated > .guten-column-resizeable > .sticky-wrapper > .guten-column-wrapper:hover > .guten-background-animated .animated-layer`
+        //     }),
+        //     tabRole: TabStyle
+        // },
+        // {
+        //     title: __('Background Overlay', '--gctd--'),
+        //     initialOpen: false,
+        //     panelArray: (props) => backgroundOverlayPanel({
+        //         ...props,
+        //         styleId: 'column-background-overlay',
+        //         normalOptions: ['default', 'gradient'],
+        //         hoverOptions: ['default', 'gradient'],
+        //         normalSelector: ` .${props.elementId} > .guten-column-resizeable > .sticky-wrapper > .guten-column-wrapper > .guten-background-overlay, .${props.elementId}.background-animated > .guten-column-resizeable > .sticky-wrapper > .guten-column-wrapper > .guten-background-overlay`,
+        //         hoverSelector: `.${props.elementId} > .guten-column-resizeable > .sticky-wrapper > .guten-column-wrapper:hover > .guten-background-overlay, .${props.elementId}.background-animated > .guten-column-resizeable > .sticky-wrapper > .guten-column-wrapper:hover > .guten-background-overlay`
+        //     }),
+        //     tabRole: TabStyle
+        // },
+        // {
+        //     title: __('Blur', '--gctd--'),
+        //     initialOpen: false,
+        //     panelArray: blurPanel,
+        //     tabRole: TabStyle
+        // },
+        // {
+        //     title: __('Border', '--gctd--'),
+        //     initialOpen: false,
+        //     panelArray: (props) => borderPanel({
+        //         ...props,
+        //         styleId: 'column-border',
+        //         selector: `.editor-styles-wrapper .is-root-container .${props.elementId} > .guten-column-resizeable > .sticky-wrapper > .guten-column-wrapper`,
+        //     }),
+        //     tabRole: TabStyle
+        // },
+        // {
+        //     title: __('Masking', '--gctd--'),
+        //     initialOpen: false,
+        //     panelArray: maskPanel,
+        //     tabRole: TabStyle
+        // },
+        // {
+        //     title: __('Pointer Event', '--gctd--'),
+        //     initialOpen: false,
+        //     panelArray: pointerEventPanel,
+        //     tabRole: TabStyle
+        // },
+        // {
+        //     title: __('Mouse Move Effect', 'gutenverse'),
+        //     initialOpen: false,
+        //     panelArray: mouseMoveEffectPanel,
+        //     tabRole: TabSetting,
+        //     pro: true,
+        // },
+        // {
+        //     title: __('Typography', '--gctd--'),
+        //     initialOpen: false,
+        //     panelArray: (props) => typographyPanel({
+        //         ...props,
+        //         styleId: 'column-typography'
+        //     }),
+        //     tabRole: TabStyle
+        // },
+        // {
+        //     title: __('Display', '--gctd--'),
+        //     initialOpen: false,
+        //     panelArray: responsivePanel,
+        //     tabRole: TabSetting
+        // },
+        // {
+        //     title: __('Animation Effects', '--gctd--'),
+        //     initialOpen: false,
+        //     panelArray: (props) => animationPanel({
+        //         ...props,
+        //         styleId: 'column-animation'
+        //     }),
+        //     tabRole: TabSetting
+        // },
+        // {
+        //     title: __('Background Animation', '--gctd--'),
+        //     initialOpen: false,
+        //     panelArray: backgroundAnimatedPanel,
+        //     pro: true,
+        // },
+        // {
+        //     title: __('Advanced Animation', '--gctd--'),
+        //     initialOpen: false,
+        //     panelAdvance: true,
+        //     panelArray: (props) => advanceAnimationPanel({
+        //         ...props,
+        //         blockType: 'column'
+        //     }),
+        //     pro: true,
+        // },
+        // {
+        //     title: __('Spacing', '--gctd--'),
+        //     initialOpen: false,
+        //     panelArray: (props) => advancePanel({
+        //         ...props,
+        //         styleId: 'column-advance',
+        //         selector: `.editor-styles-wrapper .is-root-container .${props.elementId} > .guten-column-resizeable > .sticky-wrapper > .guten-column-wrapper`
+        //     }),
+        //     tabRole: TabSetting
+        // },
+        // {
+        //     title: __('Condition', 'gutenverse'),
+        //     panelArray: conditionPanel,
+        //     initialOpen: false,
+        //     pro: true
+        // },
     ];
 };
