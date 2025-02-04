e2ae196cb7485b0ef2824e56081314e07e0d0e18
Arya1Putra <putra1arya@gmail.com>
2025-02-04 13:18:06 +0800

    [dev/inject-style-iframe] heading block js update

--- src/editor/blocks/heading/styles/block.js
+++ src/editor/blocks/heading/styles/block.js
@@ -1,111 +1,146 @@
-const getBlockStyle = (elementId) => {
-    return [
-        {
-            'type': 'plain',
-            'id': 'textAlign',
-            'selector': `h1.guten-element.${elementId},h2.guten-element.${elementId},h3.guten-element.${elementId},h4.guten-element.${elementId},h5.guten-element.${elementId},h6.guten-element.${elementId}`,
-            'property': ['text-align'],
-            'responsive': true,
-        },
-        {
-            'type': 'color',
-            'id': 'color',
-            'selector': `h1.guten-element.${elementId},h2.guten-element.${elementId},h3.guten-element.${elementId},h4.guten-element.${elementId},h5.guten-element.${elementId},h6.guten-element.${elementId}`,
-            'property': ['color'],
-        },
-        {
-            'type': 'typography',
-            'id': 'typography',
-            'selector': `h1.guten-element.${elementId},h2.guten-element.${elementId},h3.guten-element.${elementId},h4.guten-element.${elementId},h5.guten-element.${elementId},h6.guten-element.${elementId}`,
-        },
-        {
-            'type': 'textShadow',
-            'id': 'textShadow',
-            'property': ['text-shadow'],
-            'selector': `h1.guten-element.${elementId},h2.guten-element.${elementId},h3.guten-element.${elementId},h4.guten-element.${elementId},h5.guten-element.${elementId},h6.guten-element.${elementId}`,
-        },
-        {
-            'type': 'textStroke',
-            'id': 'textStroke',
-            'selector': `h1.guten-element.${elementId},h2.guten-element.${elementId},h3.guten-element.${elementId},h4.guten-element.${elementId},h5.guten-element.${elementId},h6.guten-element.${elementId}`,
-        },
-        {
-            'type': 'plain',
-            'id': 'overflowWrap',
-            'responsive': true,
-            'property': ['overflow-wrap', 'word-break'],
-            'selector': `h1.guten-element.${elementId},h2.guten-element.${elementId},h3.guten-element.${elementId},h4.guten-element.${elementId},h5.guten-element.${elementId},h6.guten-element.${elementId}`,
-        },
-        {
-            'type': 'background',
-            'id': 'background',
-            'selector': `h1.guten-element.${elementId},h2.guten-element.${elementId},h3.guten-element.${elementId},h4.guten-element.${elementId},h5.guten-element.${elementId},h6.guten-element.${elementId}`,
-        },
-        {
-            'type': 'background',
-            'id': 'backgroundHover',
-            'selector': `h1.guten-element.${elementId}:hover,h2.guten-element.${elementId}:hover,h3.guten-element.${elementId}:hover,h4.guten-element.${elementId}:hover,h5.guten-element.${elementId}:hover,h6.guten-element.${elementId}:hover`,
-        },
-        {
-            'type': 'border',
-            'id': 'border',
-            'selector': `h1.guten-element.${elementId},h2.guten-element.${elementId},h3.guten-element.${elementId},h4.guten-element.${elementId},h5.guten-element.${elementId},h6.guten-element.${elementId}`,
-        },
-        {
-            'type': 'border',
-            'id': 'borderHover',
-            'selector': `h1.guten-element.${elementId}:hover,h2.guten-element.${elementId}:hover,h3.guten-element.${elementId}:hover,h4.guten-element.${elementId}:hover,h5.guten-element.${elementId}:hover,h6.guten-element.${elementId}:hover`,
-        },
-        {
-            'type': 'borderResponsive',
-            'id': 'borderResponsive',
-            'selector': `h1.guten-element.${elementId},h2.guten-element.${elementId},h3.guten-element.${elementId},h4.guten-element.${elementId},h5.guten-element.${elementId},h6.guten-element.${elementId}`,
-        },
-        {
-            'type': 'borderResponsive',
-            'id': 'borderResponsiveHover',
-            'selector': `h1.guten-element.${elementId}:hover,h2.guten-element.${elementId}:hover,h3.guten-element.${elementId}:hover,h4.guten-element.${elementId}:hover,h5.guten-element.${elementId}:hover,h6.guten-element.${elementId}:hover`,
-        },
-        {
-            'type': 'boxShadow',
-            'id': 'boxShadow',
-            'property': ['box-shadow'],
-            'selector': `h1.guten-element.${elementId},h2.guten-element.${elementId},h3.guten-element.${elementId},h4.guten-element.${elementId},h5.guten-element.${elementId},h6.guten-element.${elementId}`,
-        },
-        {
-            'type': 'boxShadow',
-            'id': 'boxShadowHover',
-            'property': ['box-shadow'],
-            'selector': `h1.guten-element.${elementId}:hover,h2.guten-element.${elementId}:hover,h3.guten-element.${elementId}:hover,h4.guten-element.${elementId}:hover,h5.guten-element.${elementId}:hover,h6.guten-element.${elementId}:hover`,
-        },
-        {
-            'type': 'mask',
-            'id': 'mask',
-            'responsive': true,
-            'selector': `h1.guten-element.${elementId},h2.guten-element.${elementId},h3.guten-element.${elementId},h4.guten-element.${elementId},h5.guten-element.${elementId},h6.guten-element.${elementId}`,
-        },
-        {
-            'type': 'dimension',
-            'id': 'margin',
-            'responsive': true,
-            'property': ['margin'],
-            'selector': `.editor-styles-wrapper .is-root-container .${elementId}`,
-        },
-        {
-            'type': 'dimension',
-            'id': 'padding',
-            'responsive': true,
-            'property': ['padding'],
-            'selector': `.editor-styles-wrapper .is-root-container .${elementId}`,
-        },
-        {
-            'type': 'plain',
-            'id': 'zIndex',
-            'responsive': true,
-            'property': ['z-index'],
-            'selector': `.editor-styles-wrapper .is-root-container .${elementId}`,
-        },
-    ];
+import { isNotEmpty } from 'gutenverse-core/helper';
+
+const getBlockStyle = (elementId, attributes) => {
+    let data = [];
+
+    isNotEmpty(attributes['textAlign']) && data.push({
+        'type': 'plain',
+        'id': 'textAlign',
+        'selector': `h1.guten-element.${elementId},h2.guten-element.${elementId},h3.guten-element.${elementId},h4.guten-element.${elementId},h5.guten-element.${elementId},h6.guten-element.${elementId}`,
+        'property': ['text-align'],
+        'responsive': true,
+    });
+
+    isNotEmpty(attributes['color']) && data.push({
+        'type': 'color',
+        'id': 'color',
+        'selector': `h1.guten-element.${elementId},h2.guten-element.${elementId},h3.guten-element.${elementId},h4.guten-element.${elementId},h5.guten-element.${elementId},h6.guten-element.${elementId}`,
+        'property': ['color'],
+    });
+
+    isNotEmpty(attributes['typography']) && data.push({
+        'type': 'typography',
+        'id': 'typography',
+        'selector': `h1.guten-element.${elementId},h2.guten-element.${elementId},h3.guten-element.${elementId},h4.guten-element.${elementId},h5.guten-element.${elementId},h6.guten-element.${elementId}`,
+    });
+
+    isNotEmpty(attributes['textShadow']) && data.push({
+        'type': 'textShadow',
+        'id': 'textShadow',
+        'property': ['text-shadow'],
+        'selector': `h1.guten-element.${elementId},h2.guten-element.${elementId},h3.guten-element.${elementId},h4.guten-element.${elementId},h5.guten-element.${elementId},h6.guten-element.${elementId}`,
+    });
+
+    isNotEmpty(attributes['textStroke']) && data.push({
+        'type': 'textStroke',
+        'id': 'textStroke',
+        'selector': `h1.guten-element.${elementId},h2.guten-element.${elementId},h3.guten-element.${elementId},h4.guten-element.${elementId},h5.guten-element.${elementId},h6.guten-element.${elementId}`,
+    });
+
+    isNotEmpty(attributes['overflowWrap']) && data.push({
+        'type': 'plain',
+        'id': 'overflowWrap',
+        'responsive': true,
+        'property': ['overflow-wrap', 'word-break'],
+        'selector': `h1.guten-element.${elementId},h2.guten-element.${elementId},h3.guten-element.${elementId},h4.guten-element.${elementId},h5.guten-element.${elementId},h6.guten-element.${elementId}`,
+    });
+
+    isNotEmpty(attributes['background']) && data.push({
+        'type': 'background',
+        'id': 'background',
+        'selector': `h1.guten-element.${elementId},h2.guten-element.${elementId},h3.guten-element.${elementId},h4.guten-element.${elementId},h5.guten-element.${elementId},h6.guten-element.${elementId}`,
+    });
+
+    isNotEmpty(attributes['backgroundHover']) && data.push({
+        'type': 'background',
+        'id': 'backgroundHover',
+        'selector': `h1.guten-element.${elementId}:hover,h2.guten-element.${elementId}:hover,h3.guten-element.${elementId}:hover,h4.guten-element.${elementId}:hover,h5.guten-element.${elementId}:hover,h6.guten-element.${elementId}:hover`,
+    });
+
+    isNotEmpty(attributes['border']) && data.push({
+        'type': 'border',
+        'id': 'border',
+        'selector': `h1.guten-element.${elementId},h2.guten-element.${elementId},h3.guten-element.${elementId},h4.guten-element.${elementId},h5.guten-element.${elementId},h6.guten-element.${elementId}`,
+    });
+
+    isNotEmpty(attributes['borderHover']) && data.push({
+        'type': 'border',
+        'id': 'borderHover',
+        'selector': `h1.guten-element.${elementId}:hover,h2.guten-element.${elementId}:hover,h3.guten-element.${elementId}:hover,h4.guten-element.${elementId}:hover,h5.guten-element.${elementId}:hover,h6.guten-element.${elementId}:hover`,
+    });
+
+    isNotEmpty(attributes['borderResponsive']) && data.push({
+        'type': 'borderResponsive',
+        'id': 'borderResponsive',
+        'selector': `h1.guten-element.${elementId},h2.guten-element.${elementId},h3.guten-element.${elementId},h4.guten-element.${elementId},h5.guten-element.${elementId},h6.guten-element.${elementId}`,
+    });
+
+    isNotEmpty(attributes['borderResponsiveHover']) && data.push({
+        'type': 'borderResponsive',
+        'id': 'borderResponsiveHover',
+        'selector': `h1.guten-element.${elementId}:hover,h2.guten-element.${elementId}:hover,h3.guten-element.${elementId}:hover,h4.guten-element.${elementId}:hover,h5.guten-element.${elementId}:hover,h6.guten-element.${elementId}:hover`,
+    });
+
+    isNotEmpty(attributes['boxShadow']) && data.push({
+        'type': 'boxShadow',
+        'id': 'boxShadow',
+        'property': ['box-shadow'],
+        'selector': `h1.guten-element.${elementId},h2.guten-element.${elementId},h3.guten-element.${elementId},h4.guten-element.${elementId},h5.guten-element.${elementId},h6.guten-element.${elementId}`,
+    });
+
+    isNotEmpty(attributes['boxShadowHover']) && data.push({
+        'type': 'boxShadow',
+        'id': 'boxShadowHover',
+        'property': ['box-shadow'],
+        'selector': `h1.guten-element.${elementId}:hover,h2.guten-element.${elementId}:hover,h3.guten-element.${elementId}:hover,h4.guten-element.${elementId}:hover,h5.guten-element.${elementId}:hover,h6.guten-element.${elementId}:hover`,
+    });
+
+    isNotEmpty(attributes['mask']) && data.push({
+        'type': 'mask',
+        'id': 'mask',
+        'responsive': true,
+        'selector': `h1.guten-element.${elementId},h2.guten-element.${elementId},h3.guten-element.${elementId},h4.guten-element.${elementId},h5.guten-element.${elementId},h6.guten-element.${elementId}`,
+    });
+
+    isNotEmpty(attributes['padding']) && data.push({
+        'type': 'dimension',
+        'id': 'padding',
+        'responsive': true,
+        'property': ['padding'],
+        'selector': `.editor-styles-wrapper .is-root-container .${elementId}`,
+    });
+
+    isNotEmpty(attributes['margin']) && data.push({
+        'type': 'dimension',
+        'id': 'margin',
+        'responsive': true,
+        'property': ['margin'],
+        'selector': `.editor-styles-wrapper .is-root-container .${elementId}`,
+    });
+
+    isNotEmpty(attributes['zIndex']) && data.push({
+        'type': 'plain',
+        'id': 'zIndex',
+        'responsive': true,
+        'property': ['z-index'],
+        'selector': `.editor-styles-wrapper .is-root-container .${elementId}`,
+    });
+
+    isNotEmpty(attributes['animation']) && isNotEmpty(attributes['animation']['delay']) && data.push({
+        'type': 'plain',
+        'id' : 'animation',
+        'property': ['animation-delay'],
+        'selector': `.${elementId}`,
+        'valueCSS' : '{value}ms',
+        'values' : {
+            'value' : {
+                'type' : 'attribute',
+                'key'  : 'delay'
+            }
+        }
+    });
+
+    return data;
 };
 
 
