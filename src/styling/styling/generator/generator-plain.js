26a2c4e1ba544f6204a89592ff9c462719e7232d
Arya1Putra <putra1arya@gmail.com>
2025-02-04 15:04:28 +0800

    update plain generator

--- src/styling/styling/generator/generator-plain.js
+++ src/styling/styling/generator/generator-plain.js
@@ -1,9 +1,14 @@
 import { renderValue } from '../styling-helper';
 
 export const plainGenerator = (attribute, style, css) => {
-    const { type, selector, property, responsive = false } = style;
+    const { type, selector, property, responsive = false, valueCSS, values } = style;
+    let value = null;
     if (!responsive) {
-        const value = renderValue(type, attribute);
+        if( valueCSS && values ){
+            value = replaceDynamicValues(valueCSS, values, attribute);
+        }else{
+            value = renderValue(type, attribute);
+        }
         if (value) {
             const style = multiProperty(property, value);
             css.Desktop = `${selector} { ${style} }`;
@@ -12,7 +17,11 @@ export const plainGenerator = (attribute, style, css) => {
 
     if (responsive) {
         if (attribute['Desktop']) {
-            const value = renderValue(type, attribute['Desktop']);
+            if( valueCSS && values ){
+                value = replaceDynamicValues(valueCSS, values, attribute);
+            }else{
+                value = renderValue(type, attribute['Desktop']);
+            }
             if (value) {
                 const style = multiProperty(property, value);
                 css.Desktop = `${selector} { ${style} }`;
@@ -20,7 +29,11 @@ export const plainGenerator = (attribute, style, css) => {
         }
 
         if (attribute['Tablet']) {
-            const value = renderValue(type, attribute['Tablet']);
+            if( valueCSS && values ){
+                value = replaceDynamicValues(valueCSS, values, attribute);
+            }else{
+                value = renderValue(type, attribute['Tablet']);
+            }
             if (value) {
                 const style = multiProperty(property, value);
                 css.Tablet = `${selector} { ${style} }`;
@@ -28,7 +41,11 @@ export const plainGenerator = (attribute, style, css) => {
         }
 
         if (attribute['Mobile']) {
-            const value = renderValue(type, attribute['Mobile']);
+            if( valueCSS && values ){
+                value = replaceDynamicValues(valueCSS, values, attribute);
+            }else{
+                value = renderValue(type, attribute['Mobile']);
+            }
             if (value) {
                 const style = multiProperty(property, value);
                 css.Mobile = `${selector} { ${style} }`;
@@ -47,3 +64,22 @@ export const multiProperty = (properties, value) => {
     }
     return styles;
 };
+
+const replaceDynamicValues = (str, values, attribute) => {
+    let newString = str;
+    newString = newString.replace(/\{(\w+)\}/g, (_, key) => {
+        if (values[key] && values[key].type === 'attribute') {
+            return attribute[values[key].key]
+                ? attribute[values[key].key]
+                : `{${key}}`;
+        }
+        if ( values[key] && values[key].type === 'direct' ){
+            return attribute ? attribute : `{${key}}`;
+        }
+        return `{${key}}`;
+    });
+    if(newString === str){
+        return false;
+    }
+    return newString;
+}
