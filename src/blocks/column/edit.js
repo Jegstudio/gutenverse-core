4c4ca12c7ee4d1158be241cad66ad26a04271932
Yoga <yoga@jegstudio.com>
2025-03-07 16:16:57 +0800

    update hoc v2

--- src/blocks/column/edit.js
+++ src/blocks/column/edit.js
@@ -2,10 +2,10 @@ import { useRef, useState, useEffect } from '@wordpress/element';
 import { InnerBlocks, useBlockProps, Inserter, BlockControls } from '@wordpress/block-editor';
 import classnames from 'classnames';
 import { compose, debounce } from '@wordpress/compose';
-import { withCopyElementToolbar, withCursorEffect, withAnimationBackground, withMouseMoveEffect, withBackgroundEffect, withBackgroundSlideshow, withPassRef, withAnimationStickyV2, withScriptHelperV2, withAnimationAdvanceV2 } from 'gutenverse-core/hoc';
+import { withCopyElementToolbar, withCursorEffect, withAnimationBackground, withMouseMoveEffect, withBackgroundEffect, withBackgroundSlideshow, withPassRef, withAnimationStickyV2, withAnimationAdvanceV2 } from 'gutenverse-core/hoc';
 import { panelList } from './panels/panel-list';
 import { PanelController } from 'gutenverse-core/controls';
-import { removeLiveStyle, setDeviceClasses, updateLiveStyle, useDynamicStyle, useGenerateElementId } from 'gutenverse-core/styling';
+import { removeLiveStyle, setDeviceClasses, updateLiveStyle, useDynamicScript, useDynamicStyle, useGenerateElementId } from 'gutenverse-core/styling';
 import { determineLocation, isAnimationActive, isSticky, theDeviceType } from 'gutenverse-core/helper';
 import { dispatch, select, useSelect } from '@wordpress/data';
 import { useAnimationEditor, useDisplayEditor } from 'gutenverse-core/hooks';
@@ -731,7 +731,6 @@ const ColumnBlockControl = (props) => {
 const ColumnBlock = compose(
     withPassRef,
     withCopyElementToolbar(),
-    withScriptHelperV2(),
     withAnimationStickyV2(),
     withAnimationAdvanceV2('column'),
     // withAnimationBackground(),
@@ -788,6 +787,7 @@ const ColumnBlock = compose(
     const elementRef = useRef(null);
     useGenerateElementId(clientId, elementId, elementRef);
     useDynamicStyle(elementId, attributes, getBlockStyle, elementRef);
+    useDynamicScript(elementRef);
 
     const hasChildBlocks = getBlockOrder(clientId).length > 0;
     const rootClientId = getBlockRootClientId(clientId);
