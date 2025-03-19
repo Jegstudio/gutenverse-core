14df39e3d8298d22ef8d8a32b8afea1560bf9fa3
Hakim <ragamgit78@gmail.com>
2025-03-19 13:56:18 +0800

    [dev/add-close-styling-in-search] update panel-close-icon.js

--- src/editor/blocks/search/panels/panel-close-icon.js
+++ src/editor/blocks/search/panels/panel-close-icon.js
@@ -1,10 +1,10 @@
 import { __ } from '@wordpress/i18n';
 import { IconControl} from 'gutenverse-core/controls';
 
-export const iconPanel = () => {
+export const closeIconPanel = () => {
     return [
         {
-            id: 'icon',
+            id: 'closeIcon',
             label: __('Select Close Icon', 'gutenverse'),
             component: IconControl,
         },
