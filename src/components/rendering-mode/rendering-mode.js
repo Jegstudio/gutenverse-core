6156b3e51f676b65f3034b47e7deeb9ffa859915
Agung Bayu <agungbayuiswara@gmail.com>
2025-03-20 13:55:37 +0800

    set default rendering mode

---
+++ src/components/rendering-mode/rendering-mode.js
@@ -0,0 +1,25 @@
+import { registerPlugin } from '@wordpress/plugins';
+import { useDispatch, useSelect } from '@wordpress/data';
+import { useEffect } from '@wordpress/element';
+import { store as editorStore } from '@wordpress/editor';
+
+const TEMPLATE_LOCKED_POST_TYPES = ['page', 'post'];
+
+const renderingMode = registerPlugin('gutenverse-default-rendering-mode', {
+    render: function Edit() {
+        const { setRenderingMode } = useDispatch(editorStore);
+
+        const [postType] = useSelect((select) => [select(editorStore).getCurrentPostType()], []);
+
+        useEffect(() => {
+            if (!TEMPLATE_LOCKED_POST_TYPES.includes(postType)) {
+                return;
+            }
+            setRenderingMode('post-only');
+        }, [setRenderingMode, postType]);
+
+        return null;
+    },
+});
+
+export default renderingMode;
