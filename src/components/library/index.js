8d04fa9d51fb8336455d0e96f897cb8e7339af11
Yoga <yoga@jegstudio.com>
2025-04-03 14:29:13 +0800

    fix text

--- src/components/library/index.js
+++ src/components/library/index.js
@@ -74,7 +74,7 @@ const Library = () => {
                     <LogoFullWhiteNoTextSVG />
                 </div>
                 <span>
-                    {loading && open ? __('Updating Library...', '--gctd--') : __('Gutenverse Library', '--gctd--')}
+                    {loading && open ? __('Updating...', '--gctd--') : __('Gutenverse Library', '--gctd--')}
                 </span>
             </div>
         </div>
