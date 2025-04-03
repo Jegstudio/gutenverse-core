8e11541fe48c5f23153fd67a14bcd37ab324ddea
Yoga <yoga@jegstudio.com>
2025-04-03 11:08:56 +0800

    [fix/post-content-update] add popup to switch to editing mode

--- src/editor/blocks/post-content/edit.js
+++ src/editor/blocks/post-content/edit.js
@@ -1,5 +1,5 @@
 import { compose } from '@wordpress/compose';
-import { useEffect, useMemo } from '@wordpress/element';
+import { createPortal, useEffect, useState } from '@wordpress/element';
 import { withCustomStyle, withPartialRender } from 'gutenverse-core/hoc';
 import { classnames } from 'gutenverse-core/components';
 import { PanelController } from 'gutenverse-core/controls';
@@ -8,13 +8,13 @@ import { useRef } from '@wordpress/element';
 import { withCopyElementToolbar } from 'gutenverse-core/hoc';
 import { useAnimationEditor } from 'gutenverse-core/hooks';
 import { useDisplayEditor } from 'gutenverse-core/hooks';
-import { InspectorControls, RecursionProvider, useBlockProps, useInnerBlocksProps, useHasRecursion, Warning, __experimentalUseBlockPreview as useBlockPreview } from '@wordpress/block-editor';
-import { useSelect } from '@wordpress/data';
+import { InspectorControls, RecursionProvider, useBlockProps, useHasRecursion, Warning, store as blockEditorStore } from '@wordpress/block-editor';
+import { useDispatch, useSelect } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
 import { PanelTutorial } from 'gutenverse-core/controls';
 import { useSettingFallback } from 'gutenverse-core/helper';
-import { useEntityProp, useEntityBlockEditor, store as coreStore } from '@wordpress/core-data';
-import { parse } from '@wordpress/blocks';
+import { useEntityProp, store as coreStore } from '@wordpress/core-data';
+import { store as editorStore } from '@wordpress/editor';
 
 const Placeholder = () => {
     return <div className="post-content">
@@ -23,10 +23,38 @@ const Placeholder = () => {
     </div>;
 };
 
+const EditNotice = ({ setEditorWarn }) => {
+    const { setRenderingMode } = useDispatch(editorStore);
+
+    const editContent = () => {
+        setEditorWarn(false);
+        setRenderingMode('post-only');
+    };
+
+    const cancelEdit = () => {
+        setEditorWarn(false);
+    };
+
+    return <div id="gutenverse-warn">
+        <div className="gutenverse-editor-warn">
+            <div className="gutenverse-warn-wrapper">
+                <div className="gutenverse-warn-header">
+                    <span>{__('Edit Content', 'gutenverse')}</span>
+                </div>
+                <div className="gutenverse-warn-description">
+                    {__('The editor will switch to content editing. Do you want to proceed?', 'gutenverse')}
+                </div>
+                <div className="gutenverse-warn-footer">
+                    <button className="cancel" onClick={cancelEdit}>Cancel</button>
+                    <button className="primary" onClick={editContent}>Continue</button>
+                </div>
+            </div>
+        </div>
+    </div>;
+};
+
 const ReadOnlyContent = ({
-    parentLayout,
     layoutClassNames,
-    userCanEdit,
     postType,
     postId,
 }) => {
@@ -37,26 +65,6 @@ const ReadOnlyContent = ({
         postId
     );
     const blockProps = useBlockProps({ className: layoutClassNames });
-    const blocks = useMemo(() => {
-        return content?.raw ? parse(content.raw) : [];
-    }, [content?.raw]);
-    const blockPreviewProps = useBlockPreview({
-        blocks,
-        props: blockProps,
-        layout: parentLayout,
-    });
-
-    if (userCanEdit) {
-        /*
-         * Rendering the block preview using the raw content blocks allows for
-         * block support styles to be generated and applied by the editor.
-         *
-         * The preview using the raw blocks can only be presented to users with
-         * edit permissions for the post to prevent potential exposure of private
-         * block content.
-         */
-        return <div {...blockPreviewProps}></div>;
-    }
 
     return content?.protected ? (
         <div {...blockProps}>
@@ -70,50 +78,8 @@ const ReadOnlyContent = ({
     );
 };
 
-const EditableContent = ({ context = {} }) => {
-    const { postType, postId } = context;
-
-    const [blocks, onInput, onChange] = useEntityBlockEditor(
-        'postType',
-        postType,
-        { id: postId }
-    );
-
-    const entityRecord = useSelect(
-        (select) => {
-            return select(coreStore).getEntityRecord(
-                'postType',
-                postType,
-                postId
-            );
-        },
-        [postType, postId]
-    );
-
-    const hasInnerBlocks = !!entityRecord?.content?.raw || blocks?.length;
-
-    const initialInnerBlocks = [['core/paragraph']];
-
-    const props = useInnerBlocksProps(
-        {
-            className: classnames(
-                'guten-element',
-                'guten-post-content',
-            ),
-        },
-        {
-            value: blocks,
-            onInput,
-            onChange,
-            template: !hasInnerBlocks ? initialInnerBlocks : undefined,
-        },
-    );
-
-    return <div {...props} />;
-};
-
 const Content = (props) => {
-    const { context: { queryId, postType, postId } = {}, layoutClassNames } =
+    const { context: { postType, postId } = {}, layoutClassNames } =
         props;
 
     const userCanEdit = useSelect(
@@ -130,20 +96,13 @@ const Content = (props) => {
         return null;
     }
 
-    const isDescendentOfQueryLoop = Number.isFinite(queryId);
-    const isEditable = userCanEdit && !isDescendentOfQueryLoop;
-
-    return isEditable ? (
-        <EditableContent {...props} />
-    ) : (
-        <ReadOnlyContent
-            parentLayout={props.parentLayout}
-            layoutClassNames={layoutClassNames}
-            userCanEdit={userCanEdit}
-            postType={postType}
-            postId={postId}
-        />
-    );
+    return <ReadOnlyContent
+        parentLayout={props.parentLayout}
+        layoutClassNames={layoutClassNames}
+        userCanEdit={userCanEdit}
+        postType={postType}
+        postId={postId}
+    />;
 };
 
 const RecursionError = () => {
@@ -163,12 +122,25 @@ const PostContentBlock = compose(
     withCopyElementToolbar()
 )((props) => {
     const {
+        clientId,
         attributes,
         setElementRef,
         context,
         __unstableLayoutClassNames: layoutClassNames,
         __unstableParentLayout: parentLayout,
     } = props;
+    const [editorWarn, setEditorWarn] = useState(false);
+
+    const { setBlockEditingMode, unsetBlockEditingMode } = useDispatch(blockEditorStore);
+
+    useEffect(() => {
+        setBlockEditingMode(clientId, 'contentOnly');
+
+        return () => {
+            unsetBlockEditingMode(clientId);
+        };
+
+    }, [clientId, setBlockEditingMode, unsetBlockEditingMode]);
 
     const {
         elementId,
@@ -201,7 +173,10 @@ const PostContentBlock = compose(
             animationClass,
             displayClass,
         ),
-        ref: postTitleRef
+        ref: postTitleRef,
+        onClick: () => {
+            setEditorWarn(true);
+        },
     });
 
     return <>
@@ -237,6 +212,7 @@ const PostContentBlock = compose(
                 )}
             </RecursionProvider>
         </div>
+        {editorWarn && createPortal(<EditNotice setEditorWarn={setEditorWarn}/>, document.getElementById('gutenverse-root'))}
     </>;
 });
 
