64617253bf9948f9dc530e5686f62631d5e74808
Yoga <yoga@jegstudio.com>
2025-04-07 12:36:55 +0800

    [main] update notice

--- src/editor/blocks/post-content/edit.js
+++ src/editor/blocks/post-content/edit.js
@@ -1,7 +1,7 @@
 import { compose } from '@wordpress/compose';
 import { createPortal, useEffect, useMemo, useState } from '@wordpress/element';
 import { withCustomStyle, withPartialRender } from 'gutenverse-core/hoc';
-import { classnames } from 'gutenverse-core/components';
+import { classnames, Notice } from 'gutenverse-core/components';
 import { PanelController } from 'gutenverse-core/controls';
 import { panelList } from './panels/panel-list';
 import { useRef } from '@wordpress/element';
@@ -37,25 +37,16 @@ const EditNotice = ({ setEditorWarn }) => {
         setEditorWarn(false);
     };
 
-    return <div id="gutenverse-warn">
-        <div className="gutenverse-editor-warn">
-            <div className="gutenverse-warn-wrapper post-content">
-                <div className="close-icon" onClick={cancelEdit}>
-                    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M12.998 4.5493L11.4488 3L7.99805 6.52113L4.54734 3L2.99805 4.5493L6.51917 8L2.99805 11.4507L4.54734 13L7.99805 9.47887L11.4488 13L12.998 11.4507L9.47692 8L12.998 4.5493Z" fill="#99A2A9" />
-                    </svg>
-                </div>
-                <div className="lock-icon">
-                    <svg width="24" height="24" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path fillRule="evenodd" clipRule="evenodd" d="M5.83268 5.83464C5.83268 3.53297 7.69768 1.66797 9.99935 1.66797C12.301 1.66797 14.166 3.53297 14.166 5.83464V8.33464H14.4993C15.2327 8.33464 15.8327 8.93464 15.8327 9.66797V15.5013C15.8327 16.6013 14.9327 17.5013 13.8327 17.5013H6.16602C5.06602 17.5013 4.16602 16.6013 4.16602 15.5013V9.66797C4.16602 8.93464 4.76602 8.33464 5.49935 8.33464H5.83268V5.83464ZM12.4993 5.83464V8.33464H7.49935V5.83464C7.49935 4.45297 8.61768 3.33464 9.99935 3.33464C11.381 3.33464 12.4993 4.45297 12.4993 5.83464ZM9.99935 10.2096C9.66796 10.2093 9.34634 10.3218 9.08746 10.5287C8.82858 10.7356 8.64787 11.0244 8.5751 11.3477C8.50232 11.671 8.54183 12.0095 8.68711 12.3073C8.83239 12.6051 9.07478 12.8446 9.37435 12.9863V15.0013C9.37435 15.1671 9.4402 15.326 9.55741 15.4432C9.67462 15.5605 9.83359 15.6263 9.99935 15.6263C10.1651 15.6263 10.3241 15.5605 10.4413 15.4432C10.5585 15.326 10.6243 15.1671 10.6243 15.0013V12.9863C10.9239 12.8446 11.1663 12.6051 11.3116 12.3073C11.4569 12.0095 11.4964 11.671 11.4236 11.3477C11.3508 11.0244 11.1701 10.7356 10.9112 10.5287C10.6524 10.3218 10.3307 10.2093 9.99935 10.2096Z" fill="#FFB200" />
-                    </svg>
-                </div>
-                <h3>{__('Gutenverse Post Content is Locked', 'gutenverse')}</h3>
-                <p>{__('On "Show Template" mode, Gutenverse Post Content cannot be edited. Please switch to Content Editing mode to edit the content.', 'gutenverse')}</p>
-                <button className="primary" onClick={editContent}>{__('Switch Mode', 'gutenverse')}</button>
-            </div>
-        </div>
-    </div>;
+    return <Notice
+        icon={<svg width="24" height="24" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fillRule="evenodd" clipRule="evenodd" d="M5.83268 5.83464C5.83268 3.53297 7.69768 1.66797 9.99935 1.66797C12.301 1.66797 14.166 3.53297 14.166 5.83464V8.33464H14.4993C15.2327 8.33464 15.8327 8.93464 15.8327 9.66797V15.5013C15.8327 16.6013 14.9327 17.5013 13.8327 17.5013H6.16602C5.06602 17.5013 4.16602 16.6013 4.16602 15.5013V9.66797C4.16602 8.93464 4.76602 8.33464 5.49935 8.33464H5.83268V5.83464ZM12.4993 5.83464V8.33464H7.49935V5.83464C7.49935 4.45297 8.61768 3.33464 9.99935 3.33464C11.381 3.33464 12.4993 4.45297 12.4993 5.83464ZM9.99935 10.2096C9.66796 10.2093 9.34634 10.3218 9.08746 10.5287C8.82858 10.7356 8.64787 11.0244 8.5751 11.3477C8.50232 11.671 8.54183 12.0095 8.68711 12.3073C8.83239 12.6051 9.07478 12.8446 9.37435 12.9863V15.0013C9.37435 15.1671 9.4402 15.326 9.55741 15.4432C9.67462 15.5605 9.83359 15.6263 9.99935 15.6263C10.1651 15.6263 10.3241 15.5605 10.4413 15.4432C10.5585 15.326 10.6243 15.1671 10.6243 15.0013V12.9863C10.9239 12.8446 11.1663 12.6051 11.3116 12.3073C11.4569 12.0095 11.4964 11.671 11.4236 11.3477C11.3508 11.0244 11.1701 10.7356 10.9112 10.5287C10.6524 10.3218 10.3307 10.2093 9.99935 10.2096Z" fill="#FFB200" />
+        </svg>}
+        title={__('Gutenverse Post Content is Locked.', '--gctd--')}
+        description={__('On "Show Template" mode, Gutenverse Post Content cannot be edited. Please switch to Content Editing mode to edit the content.', '--gctd--')}
+        buttonText={__('Switch Mode', '--gctd--')}
+        onCLick={editContent}
+        onClose={cancelEdit}
+    />;
 };
 
 const ReadOnlyContent = ({
