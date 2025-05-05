f3a07414bff3fa8597300611ab55fa5fb034d256
Arya1Putra <putra1arya@gmail.com>
2025-05-05 10:20:05 +0800

    [fix/fix-bug] fix post block frontend pagination

--- gutenverse/includes/class-api.php
+++ gutenverse/includes/class-api.php
@@ -198,7 +198,7 @@ class Api {
 
 		$post_data->set_attributes( $attributes );
 
-		$render = $post_data->render_content( false, $attributes['excludeCurrentPost'] );
+		$render = $post_data->render_frontend( false, $attributes['excludeCurrentPost'] );
 
 		return array(
 			'rendered' => $render,
