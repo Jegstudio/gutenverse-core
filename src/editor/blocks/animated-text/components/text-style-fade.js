ab0dcd9e6ebd3415226399d029351d6037fde346
Hakim <ragamgit78@gmail.com>
2025-05-21 18:02:47 +0800

    [dev/animated-text] update Fade Animation

--- src/editor/blocks/animated-text/components/text-style-fade.js
+++ src/editor/blocks/animated-text/components/text-style-fade.js
@@ -1,54 +1,64 @@
 import anime from 'animejs';
-import { useEffect, useState } from '@wordpress/element';
+import { useEffect, useRef, useState } from '@wordpress/element';
 import { u } from 'gutenverse-core/components';
 
 const TextStyleFade = (props) => {
     const {
         text,
-        titleTag: TitleTag,
         loop,
         animatedTextRef,
         splitByWord,
-        style
+        style,
+        resetText,
+        animationDuration,
+        displayDuration,
     } = props;
 
     const [animation, setAnimation] = useState();
+    const timeoutRef = useRef(null);
 
-    const animeInit = () => {
-        const textWrapper = u(animatedTextRef.current).find('.text-content');
-        textWrapper.html(textWrapper.text().replace(splitByWord ? /\b\w+\b/g : /\S/g, (word) => `<span class='letter'>${word}</span>`));
+    const loopAnimation = () => {
+        timeoutRef.current = setTimeout(() => {
+            resetText();
+            fadeAnimation();
+        }, displayDuration);
+    };
 
-        const animeInit = anime.timeline({ loop })
-            .add({
-                targets: [...animatedTextRef.current.getElementsByClassName('letter')],
-                opacity: [0, 1],
-                easing: 'easeInOutQuad',
-                duration: 2250,
-                delay: (el, i) => 150 * (i + 1)
-            });
-
-        loop && animeInit.add({
-            targets: [...animatedTextRef.current.getElementsByClassName('text-content')],
-            opacity: 0,
-            duration: 1000,
-            easing: 'easeOutExpo',
-            delay: 1000
+    const fadeAnimation = () => {
+        anime.remove('.letter');
+        const animeInstance = anime({
+            targets: [...animatedTextRef.current.getElementsByClassName('letter')],
+            opacity: [0, 1],
+            easing: 'easeInOutQuad',
+            duration: animationDuration,
+            delay: (el, i) => 70 * i,
+            complete: () => {
+                if (loop) {
+                    loopAnimation();
+                }
+            }
         });
 
-        setAnimation(animeInit);
+        setAnimation(animeInstance);
     };
 
     useEffect(() => {
-        animeInit();
+        if (animation) {
+            anime.remove('.letter');
+            setAnimation(null);
+        }
+        resetText();
+        fadeAnimation();
+
         return () => {
-            if (animation) {
-                animation.remove();
-                setAnimation(null);
+            anime.remove('.letter');
+            if (timeoutRef.current) {
+                clearTimeout(timeoutRef.current);
             }
         };
-    }, [loop, splitByWord, style]);
+    }, [loop, splitByWord, style, animationDuration, displayDuration, text]);
 
-    return <TitleTag className="text-content">{text}</TitleTag>;
+    return <span className="text-content">{text}</span>;
 };
 
 export default TextStyleFade;
