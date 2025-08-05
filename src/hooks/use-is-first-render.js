4331ff076d3c8586b918e6d6dc165a328f0b3a12
Hakim <ragamgit78@gmail.com>
2025-08-05 12:05:19 +0800

    add since in useIsFirstRender

--- src/hooks/use-is-first-render.js
+++ src/hooks/use-is-first-render.js
@@ -1,5 +1,11 @@
 import { useEffect, useRef } from '@wordpress/element';
 
+/**
+ * Check is first render for component
+ *
+ * @since 2.1.1
+ * @returns {boolean}
+ */
 export default function useIsFirstRender() {
     const isFirst = useRef(true);
     useEffect(() => {
