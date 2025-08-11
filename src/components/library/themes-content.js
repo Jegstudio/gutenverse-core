e92434f8fc94298a1ee09fc400ab4e7601ea4284
Arya1Putra <putra1arya@gmail.com>
2025-08-11 16:06:33 +0800

    install and activate unibiz and companion from library done

--- src/components/library/themes-content.js
+++ src/components/library/themes-content.js
@@ -4,21 +4,41 @@ import { useSelect, dispatch, withSelect } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
 import { IconArrowRightSVG, IconDownloadSVG, IconLibraryThemeListSVG, IconLoadingSVG } from 'gutenverse-core/icons';
 import apiFetch from '@wordpress/api-fetch';
+import { installAndActivateTheme, installingPlugins } from 'gutenverse-core/helper';
 
 const ThemesContent = (props) => {
-    const { imgDir, proDemoUrl } = window['GutenverseConfig'] || {};
+    const { imgDir, proDemoUrl, domainURL } = window['GutenverseConfig'] || {};
     const [buttonText, setButtonText] = useState(__('Install Unibiz Theme', 'gutenverse'));
     const activateTheme = (slug) => {
-        setButtonText(<IconLoadingSVG/>);
-        setTimeout(() => {
-            setButtonText(__('Installed and Activated', 'gutenverse'));
-        }, 500);
+        setButtonText(<IconLoadingSVG />);
+        const themeSlug = 'unibiz'; // change this to your theme slug
+        const pluginsList = [
+            { name: 'Gutenverse Companion', slug: 'gutenverse-companion', version: '', url: '' },
+        ];
+
+        // Step 1: Install + Activate Theme
+        installAndActivateTheme(themeSlug)
+            .then(themeResponse => {
+                console.log('Theme installed and activated:', themeResponse);
+                // Step 2: Install + Activate Plugins
+                return installingPlugins(pluginsList);
+            })
+            .then(() => {
+                console.log('All plugins installed and activated!');
+                window.location.replace(`${domainURL}/wp-admin/admin.php?page=gutenverse-companion-dashboard&path=demo`);
+            })
+            .catch(err => {
+                console.error('Installation failed:', err);
+                alert('Something went wrong during installation.');
+                setButtonText(__('Install Unibiz Theme', 'gutenverse'));
+            });
     };
     return <>
         <div id="gutenverse-library-themes-content-wrapper">
             <div className="banner-wrapper">
                 <img className="background-banner" src={`${imgDir}/wizard-bg-cta-companion.png`} alt="background" />
                 <div className="banner-content">
+                    <div className="banner-glitter" style={{ backgroundImage: `url(${imgDir}/wizard-bg-confetti-unibiz.png)` }}></div>
                     <div className="col-1">
                         <div className="title-wrapper">
                             <h2 className="title">{__('Supercharge Gutenverse', 'gutenverse')}</h2>
@@ -45,16 +65,15 @@ const ThemesContent = (props) => {
                             </div>
                             <div className="action-wrapper">
                                 <div className="button-install-theme" onClick={activateTheme}>{buttonText}</div>
-                                <a className="button-learn-more" href={`${proDemoUrl}/unibiz/`} target="_blank" rel="noreferrer">{__('Learn More', 'gutenverse')} <IconArrowRightSVG /></a>
+                                <a className="button-learn-more" href={`${proDemoUrl}/unibiz/`} target="_blank" rel="noreferrer">{__('Learn More', 'gutenverse')}</a>
                             </div>
+                            <p className="notice-text">{__('By clicking “Install Unibiz Theme” I agree to install & activate Gutenverse Companion plugin.', 'gutenverse')}</p>
                         </div>
                     </div>
-                    <div className="col-2">
-                        <img className="mockup-demo-image" src={`${imgDir}/wizard-mockup-demo-companion.png`} alt="computer" />
+                    <div className="col-2" style={{ backgroundImage: `url(${imgDir}/wizard-bg-list-demo.png)` }}>
+                        <img className="mockup-demo-image" src={`${imgDir}/wizard-mockup-demo-companion-with-shadow.png`} alt="computer" />
                     </div>
                 </div>
-                <img className="list-demo-image" src={`${imgDir}/wizard-bg-list-demo.png`} alt="computer" />
-                <img className="banner-glitter" src={`${imgDir}/wizard-bg-confetti-unibiz.png`} alt="confetti" />
             </div>
         </div >
     </>;
