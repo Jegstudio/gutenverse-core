708ea5a9a49fe55e358f53cb55418aca7a0f7fea
Arya1Putra <putra1arya@gmail.com>
2025-08-29 14:14:46 +0800

    change button upgrade text from 'Avaiable in Pro Version' to 'Upgrade to Pro'

--- src/controls/controls/locked/default-layout.js
+++ src/controls/controls/locked/default-layout.js
@@ -20,7 +20,7 @@ const DefaultLayout = ({
 
     const ButtonPro = applyFilters(
         'gutenverse.pro-panel-button',
-        () => isEmpty(window?.gprodata) && <a className="gutenverse-button-available-pro" href={`${upgradeProUrl}?utm_source=gutenverse&utm_medium=blockeditor&utm_client_site=${clientUrl}&utm_client_theme=${activeTheme}`} target="_blank" rel="noreferrer"> {__('Available in Pro Version', '--gctd--')} <IconCrownBannerSVG /> </a>
+        () => isEmpty(window?.gprodata) && <a className="gutenverse-button-available-pro" href={`${upgradeProUrl}?utm_source=gutenverse&utm_medium=blockeditor&utm_client_site=${clientUrl}&utm_client_theme=${activeTheme}`} target="_blank" rel="noreferrer"> {__('Upgrade To Pro', '--gctd--')} <IconCrownBannerSVG /> </a>
     );
     return <>
         <h2 className="title">{title}</h2>
