e41e0be152da00d405ab6ed4ac70b94bd841dbe2
Agung Bayu <agungbayuiswara@gmail.com>
2025-10-23 11:58:03 +0800

    toolbar sesuai design

--- framework/includes/class-frontend-toolbar.php
+++ framework/includes/class-frontend-toolbar.php
@@ -127,22 +127,6 @@ class Frontend_Toolbar {
 					)
 				);
 
-				$site_url     = get_site_url();
-				$active_theme = get_option( 'stylesheet' );
-				if ( ! defined( 'GUTENVERSE_PRO' ) ) {
-					$admin_bar->add_menu(
-						array(
-							'id'     => 'gutenverse-pro',
-							'parent' => 'gutenverse',
-							'title'  => '<span class="gutenverse-pro-right">' . esc_html__( 'Upgrade to PRO', '--gctd--' ) . '<img src="' . esc_url( GUTENVERSE_FRAMEWORK_URL_PATH . '/assets/icon/icon-crown.svg' ) . '"/> </span>',
-							'href'   => gutenverse_upgrade_pro() . '/?utm_source=gutenverse&utm_medium=admintopbar&utm_client_site=' . $site_url . '&utm_client_theme=' . $active_theme,
-							'meta'   => array(
-								'target' => '_blank',
-							),
-						)
-					);
-				}
-
 				if ( ! is_admin() ) {
 					if ( current_theme_supports( 'block-templates' ) && $this->is_block_template ) {
 						$block_template = resolve_block_template(
@@ -158,7 +142,7 @@ class Frontend_Toolbar {
 
 					$admin_bar->add_menu(
 						array(
-							'id'     => 'space',
+							'id'     => 'first-space',
 							'parent' => 'gutenverse',
 							'title'  => '',
 						)
@@ -195,6 +179,30 @@ class Frontend_Toolbar {
 		}
 
 		$this->setting_toolbar( $admin_bar, 'backend' );
+		$this->pro_toolbar( $admin_bar );
+	}
+
+	/**
+	 * Pro Toolbar
+	 *
+	 * @param \WP_Admin_Bar $admin_bar Admin Bar Instance.
+	 */
+	public function pro_toolbar( $admin_bar ) {
+		$site_url     = get_site_url();
+		$active_theme = get_option( 'stylesheet' );
+		if ( ! defined( 'GUTENVERSE_PRO' ) ) {
+			$admin_bar->add_menu(
+				array(
+					'id'     => 'gutenverse-pro',
+					'parent' => 'gutenverse',
+					'title'  => '<span class="gutenverse-pro-right">' . esc_html__( 'Upgrade to PRO', '--gctd--' ) . '<img src="' . esc_url( GUTENVERSE_FRAMEWORK_URL_PATH . '/assets/icon/icon-crown.svg' ) . '"/> </span>',
+					'href'   => gutenverse_upgrade_pro() . '/?utm_source=gutenverse&utm_medium=admintopbar&utm_client_site=' . $site_url . '&utm_client_theme=' . $active_theme,
+					'meta'   => array(
+						'target' => '_blank',
+					),
+				)
+			);
+		}
 	}
 
 	/**
@@ -261,6 +269,26 @@ class Frontend_Toolbar {
 
 		$admin_bar->add_menu(
 			array(
+				'id'     => 'second-space',
+				'parent' => 'gutenverse',
+				'title'  => '',
+			)
+		);
+
+		$admin_bar->add_menu(
+			array(
+				'id'     => 'rate',
+				'parent' => 'gutenverse',
+				'title'  => esc_html__( 'Rate Us ', '--gctd--' ) . '<span>★★★★★</span>',
+				'href'   => 'https://wordpress.org/support/plugin/gutenverse/reviews/#new-post',
+				'meta'   => array(
+					'target' => '_blank',
+				),
+			)
+		);
+
+		$admin_bar->add_menu(
+			array(
 				'id'     => 'gutenverse-template',
 				'parent' => 'gutenverse-site-editor',
 				'title'  => esc_html__( 'All Template', '--gctd--' ),
@@ -291,21 +319,9 @@ class Frontend_Toolbar {
 
 		$admin_bar->add_menu(
 			array(
-				'id'     => 'rate',
-				'parent' => 'gutenverse',
-				'title'  => esc_html__( 'Rate Us ★★★★★', '--gctd--' ),
-				'href'   => 'https://wordpress.org/support/plugin/gutenverse/reviews/#new-post',
-				'meta'   => array(
-					'target' => '_blank',
-				),
-			)
-		);
-
-		$admin_bar->add_menu(
-			array(
 				'id'     => 'help-documentation',
 				'parent' => 'gutenverse',
-				'title'  => esc_html__( 'Help/Documentation', '--gctd--' ),
+				'title'  => esc_html__( 'Documentation', '--gctd--' ),
 				'href'   => 'https://gutenverse.com/docs/',
 				'meta'   => array(
 					'target' => '_blank',
