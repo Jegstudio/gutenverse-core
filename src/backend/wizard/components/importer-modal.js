c8e1ffc44db2dc0753224409d79dc96009ebf0f2
Yoga <yoga@jegstudio.com>
2025-10-24 10:23:06 +0800

    [trunk] small fix css

--- src/backend/wizard/components/importer-modal.js
+++ src/backend/wizard/components/importer-modal.js
@@ -259,7 +259,7 @@ const ModalContent = ({
                     </div>
                     <div className="importer-step">
                         <div className="import-step-notice">
-                            {importerNotice}
+                            <p>{importerNotice}</p>
                         </div>
                         <div className="importer-inner-step">
                             {importerStep.map((step, index) => {
