771f0b56af5ff5ae65159a1dfe208adcac2ff645
Yoga <yoga@jegstudio.com>
2025-11-07 17:41:11 +0800

    remove wordpress scripts from core frontend

--- src/frontend/helper/fetch.js
+++ src/frontend/helper/fetch.js
@@ -1,11 +1,13 @@
-import { apiFetch } from '../index';
+// import apiFetch from '@wordpress/api-fetch';
 
 // Initialize Fetch so it can be used on each frontend plugin.
-export default (function (apiFetch) {
-    const { wpjson_url, wpjson_nonce, wpjson_endpoint } = window.GutenverseFrontendConfig;
-    apiFetch.use(apiFetch.createRootURLMiddleware(wpjson_url));
-    apiFetch.nonceMiddleware = apiFetch.createNonceMiddleware(wpjson_nonce);
-    apiFetch.use(apiFetch.nonceMiddleware);
-    apiFetch.use(apiFetch.mediaUploadMiddleware);
-    apiFetch.nonceEndpoint = wpjson_endpoint;
-})(apiFetch);
+// export default (function (apiFetch) {
+//     const { wpjson_url, wpjson_nonce, wpjson_endpoint } = window.GutenverseFrontendConfig;
+//     apiFetch.use(apiFetch.createRootURLMiddleware(wpjson_url));
+//     apiFetch.nonceMiddleware = apiFetch.createNonceMiddleware(wpjson_nonce);
+//     apiFetch.use(apiFetch.nonceMiddleware);
+//     apiFetch.use(apiFetch.mediaUploadMiddleware);
+//     apiFetch.nonceEndpoint = wpjson_endpoint;
+// })(apiFetch);
+
+// comment for now for archive
--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,15 +1,8 @@
+import { ReplaceCSS } from './blocks/replace-css';
+
 export { Default } from './blocks/default';
 export { default as u } from 'umbrellajs';
 export { default as load } from 'load-script';
-export { default as apiFetch } from '@wordpress/api-fetch';
 export { responsiveBreakpoint } from './helper/index';
-import { ReplaceCSS } from './blocks/replace-css';
-
 export default (new ReplaceCSS()).init();
-
-export * from '@wordpress/hooks';
-export * from '@wordpress/url';
-
-// Internal.
-export * from './helper/fetch';
 export * from './scroll';
