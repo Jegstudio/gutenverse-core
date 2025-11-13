2ee9c4db8625bca2fbdaa1a2fa5d610da2780b11
Yoga <yoga@jegstudio.com>
2025-11-13 10:05:29 +0800

    update version

--- framework/includes/class-dashboard.php
+++ framework/includes/class-dashboard.php
@@ -524,6 +524,10 @@ class Dashboard {
 					'plugin_version'    => '3.2.1',
 					'framework_version' => '2.2.1',
 				),
+				array(
+					'plugin_version'    => '3.3.0',
+					'framework_version' => '2.3.0',
+				),
 			),
 			'gutenverse-form' => array(
 				array(
@@ -654,6 +658,10 @@ class Dashboard {
 					'plugin_version'    => '2.2.0',
 					'framework_version' => '2.2.0',
 				),
+				array(
+					'plugin_version'    => '2.3.0',
+					'framework_version' => '2.3.0',
+				),
 			),
 			'gutenverse-news' => array(
 				array(
@@ -761,6 +769,10 @@ class Dashboard {
 				array(
 					'plugin_version'    => '2.2.1',
 					'framework_version' => '2.2.1',
+				),
+				array(
+					'plugin_version'    => '2.3.0',
+					'framework_version' => '2.3.0',
 				),
 			),
 		);
