1d154b9399fbd57b0aa32c87bb69dc2f1b5f60c7
Agung Bayu <agungbayuiswara@gmail.com>
2025-12-07 13:44:01 +0800

    default svg styling

--- src/helper/index.js
+++ src/helper/index.js
@@ -960,3 +960,32 @@ export const svgAtob = (encodedSVG) => {
         console.error('Invalid base64:', e);
     }
 };
+
+/**
+ * Render icon (font icon or SVG)
+ * @param {string} icon - Icon class name
+ * @param {string} iconType - Type of icon ('icon' or 'svg')
+ * @param {string} iconSVG - Base64 encoded SVG data
+ * @returns {JSX.Element|null} - Rendered icon element
+ */
+export const renderIcon = (icon, iconType = 'icon', iconSVG = '') => {
+    if (iconType === 'svg' && iconSVG) {
+        try {
+            const svgData = atob(iconSVG);
+            return (
+                <div
+                    className="gutenverse-icon-svg"
+                    dangerouslySetInnerHTML={{ __html: svgData }}
+                />
+            );
+        } catch (e) {
+            return null;
+        }
+    }
+
+    if (icon) {
+        return <i className={icon}></i>;
+    }
+
+    return null;
+};
