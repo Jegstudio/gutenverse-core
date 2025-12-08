931ce8174cb33d85f6a79441bbbd140c85b96ae1
Agung Bayu <agungbayuiswara@gmail.com>
2025-12-08 11:15:43 +0800

    use iconControl as fallback

--- src/controls/controls/icon/icon-svg-control.js
+++ src/controls/controls/icon/icon-svg-control.js
@@ -7,12 +7,13 @@ import gtnIcons from './gutenverse-icons';
 import { __ } from '@wordpress/i18n';
 import { Trash, X } from 'react-feather';
 import { Button } from '@wordpress/components';
+import { MediaUpload } from '@wordpress/block-editor';
 import classnames from 'classnames';
 import { compose } from '@wordpress/compose';
 import { withParentControl } from 'gutenverse-core/hoc';
 import { withDeviceControl } from 'gutenverse-core/hoc';
 import { AutoSizer, Grid } from 'react-virtualized';
-import { gutenverseRoot } from 'gutenverse-core/helper';
+import { svgAtob, gutenverseRoot } from 'gutenverse-core/helper';
 import { LogoFullColorSVG, IconSearchSVG } from 'gutenverse-core/icons';
 
 const COLUMN_NUMBER = 8;
@@ -168,7 +169,7 @@ export const IconLibrary = ({
                 <div className={'icon-library-search'}>
                     <div className={'input'}>
                         <IconSearchSVG />
-                        <input type={'text'} placeholder={__('Search Icon', '--gctd--')} onChange={changeFilter} autoFocus={true}/>
+                        <input type={'text'} placeholder={__('Search Icon', '--gctd--')} onChange={changeFilter} autoFocus={true} />
                     </div>
                 </div>
                 <div className={'icon-library-result'}>
@@ -195,15 +196,34 @@ export const IconLibrary = ({
     </div>;
 };
 
-const IconControl = ({
-    label,
-    allowDeviceControl,
-    value = '',
-    onValueChange,
-    description = '',
-}) => {
+const IconSVGControl = (props) => {
+    const {
+        label,
+        allowDeviceControl,
+        value = '',
+        onValueChange,
+        attributes,
+        setAttributes,
+        id,
+        values,
+    } = props;
+
     const [openIconLibrary, setOpenIconLibrary] = useState(false);
-    const id = useInstanceId(IconControl, 'inspector-icon-control');
+    const instanceId = useInstanceId(IconSVGControl, 'inspector-icon-control');
+
+    const typeAttribute = id ? `${id}Type` : '';
+    const svgAttribute = id ? `${id}SVG` : '';
+
+    const iconType = (attributes && attributes[typeAttribute]) ? attributes[typeAttribute] : (values && values[typeAttribute] ? values[typeAttribute] : 'icon');
+    const svgValue = (attributes && attributes[svgAttribute]) ? attributes[svgAttribute] : (values && values[svgAttribute] ? values[svgAttribute] : {});
+
+    const updateAttributes = (newAttributes) => {
+        if (setAttributes) {
+            setAttributes(newAttributes);
+        } else if (values && values.setAttributes) {
+            values.setAttributes(newAttributes);
+        }
+    };
 
     const onChange = value => {
         onValueChange(value);
@@ -214,26 +234,99 @@ const IconControl = ({
         onChange('');
     };
 
-    return <div id={id} className={'gutenverse-control-wrapper gutenverse-control-icon'}>
+    const removeSVG = (e) => {
+        e.stopPropagation();
+        if (svgAttribute) {
+            updateAttributes({ [svgAttribute]: '' });
+        }
+    };
+
+    const onSelectSVG = (media) => {
+        if (svgAttribute && media.url) {
+            // Fetch SVG content and store as string
+            fetch(media.url)
+                .then(response => response.text())
+                .then(svgContent => {
+                    const encodedSVG = btoa(svgContent);
+                    updateAttributes({ [svgAttribute]: encodedSVG });
+                })
+                .catch((error) => {
+                    console.error('Failed to fetch SVG content:', error);
+                    // Fallback: store empty string
+                    updateAttributes({ [svgAttribute]: '' });
+                });
+        }
+    };
+
+    const setType = (type) => {
+        if (typeAttribute) {
+            updateAttributes({ [typeAttribute]: type });
+        }
+    };
+
+    return <div id={instanceId} className={'gutenverse-control-wrapper gutenverse-control-icon'}>
         <ControlHeadingSimple
-            id={`${id}-icon`}
+            id={`${instanceId}-icon`}
             label={label}
-            description={description}
+            description={__('Using the icon library may increase your frontend size. For best performance, use uploaded SVGs for all icons.', '--gctd--')}
             allowDeviceControl={allowDeviceControl}
         />
-        <div className={'control-body'}>
-            <div className={'icon-wrapper'}>
-                {value !== '' && <div className={'icon-remove'} onClick={e => removeIcon(e)}>
-                    <Trash />
-                </div>}
-                <div className={'icon-preview'} onClick={() => setOpenIconLibrary(true)}>
-                    <i className={value} />
-                </div>
-                <div className={'icon-change'} onClick={() => setOpenIconLibrary(true)}>
-                    {__('Choose Icon', '--gctd--')}
+
+        {iconType === 'icon' ? (
+            <div className={'control-body'}>
+                <div>
+                    <div className={'icon-wrapper'}>
+                        {value !== '' && <div className={'icon-remove'} onClick={e => removeIcon(e)}>
+                            <Trash />
+                        </div>}
+                        <div className={'icon-preview'} onClick={() => setOpenIconLibrary(true)}>
+                            <i className={value} />
+                        </div>
+                        <div className={'icon-change'}>
+                            <div className={'choose-icon active'} onClick={() => setType('icon')}>
+                                {__('Choose Icon', '--gctd--')}
+                            </div>
+                            <div className={'upload-svg'} onClick={() => setType('svg')}>
+                                {__('Upload SVG', '--gctd--')}
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+        ) : (
+            <div className={'control-body'}>
+                <MediaUpload
+                    onSelect={onSelectSVG}
+                    allowedTypes={['image/svg+xml']}
+                    value={undefined}
+                    render={({ open }) => (
+                        <div className={'icon-wrapper'}>
+                            {svgValue && <div className={'icon-remove'} onClick={e => removeSVG(e)}>
+                                <Trash />
+                            </div>}
+                            <div className={'icon-preview'} onClick={open} style={{
+                                backgroundSize: '20px 20px',
+                                backgroundPosition: '0 0, 10px 10px',
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'center'
+                            }}>
+                                {svgValue ? <div dangerouslySetInnerHTML={{ __html: svgAtob(svgValue) }} style={{ maxWidth: '60%', maxHeight: '60%', display: 'flex' }} /> : null}
+                            </div>
+                            <div className={'icon-change'}>
+                                <div className={'choose-icon'} onClick={() => setType('icon')}>
+                                    {__('Choose Icon', '--gctd--')}
+                                </div>
+                                <div className={'upload-svg active'} onClick={() => setType('svg')}>
+                                    {__('Upload SVG', '--gctd--')}
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                />
+            </div>
+        )}
+
         {openIconLibrary && createPortal(<IconLibrary
             closeLibrary={() => setOpenIconLibrary(false)}
             value={value}
@@ -242,4 +335,4 @@ const IconControl = ({
     </div>;
 };
 
-export default compose(withParentControl, withDeviceControl)(IconControl);
+export default compose(withParentControl, withDeviceControl)(IconSVGControl);
