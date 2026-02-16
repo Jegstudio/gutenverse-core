a710069d6d2a7c9726d656a24607cfc74795f54e
Ikhlashmulya <nurahmanmulya@gmail.com>
2026-02-16 17:46:30 +0800

    uncomment like dislike button title

--- src/dashboard/content/settings/tabs.js
+++ src/dashboard/content/settings/tabs.js
@@ -60,7 +60,7 @@ const setitngTitle = {
     view_counter: __('View Counter Settings', '--gctd--'),
     post_review: __('Post Review Settings', '--gctd--'),
     paywall: __('Paywall Settings', '--gctd--'),
-    // like_dislike_button: __('Like & Dislike Settings', '--gctd--'),
+    like_dislike_button: __('Like & Dislike Settings', '--gctd--'),
     performance: __('Performance Settings', '--gctd--'),
 };
 
