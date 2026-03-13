ddda6f570d73df0dc9089119562bfca23e796039
Arya1Putra <putra1arya@gmail.com>
2026-03-13 10:35:32 +0800

    remove condition only for themeforest liteplus

--- src/controls/controls/panel/panel-tab-pro.js
+++ src/controls/controls/panel/panel-tab-pro.js
@@ -45,7 +45,7 @@ const PanelTabPro = ({ activeTab }) => {
                                 </div>
                             }
                             {
-                                (adsBannerThemeTF && isUsingGutenverseThemeTF && today <= new Date(adsBannerThemeTF?.fse_dashboard?.expired)) && <div className="event-banner-wrapper">
+                                (adsBannerThemeTF && today <= new Date(adsBannerThemeTF?.fse_dashboard?.expired)) && <div className="event-banner-wrapper">
                                     <a href={`${adsBannerThemeTF?.fse_dashboard?.url}?utm_source=gutenverse&utm_medium=blockeditor&utm_client_site=${clientUrl}&utm_client_theme=${activeTheme}`} target="_blank" rel="noreferrer" >
                                         <img src={adsBannerThemeTF?.fse_dashboard?.img} alt="event-banner" />
                                     </a>
