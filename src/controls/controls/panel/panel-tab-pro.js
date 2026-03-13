c81ff951f0b16bbc972c93537780693261c03728
Arya1Putra <putra1arya@gmail.com>
2026-03-13 10:41:37 +0800

    remove unused

--- src/controls/controls/panel/panel-tab-pro.js
+++ src/controls/controls/panel/panel-tab-pro.js
@@ -1,14 +1,14 @@
 import { applyFilters } from '@wordpress/hooks';
 import React from 'react';
 import { __ } from '@wordpress/i18n';
-import { IconCrownBannerSVG, IconCrownSVG, IconKeySVG } from 'gutenverse-core/icons';
+import { IconCrownBannerSVG, IconKeySVG } from 'gutenverse-core/icons';
 import isEmpty from 'lodash/isEmpty';
 import { ButtonUpgradePro } from 'gutenverse-core/components';
 import { activeTheme, clientUrl } from 'gutenverse-core/config';
 
 const PanelTabPro = ({ activeTab }) => {
     const {
-        upgradeProUrl, adminUrl, eventBanner, adsBannerThemeTF, isUsingGutenverseThemeTF
+        upgradeProUrl, adminUrl, eventBanner, adsBannerThemeTF
     } = window['GutenverseConfig'] || window['GutenverseDashboard'] || {};
     const eventData = eventBanner;
     const today = new Date();
