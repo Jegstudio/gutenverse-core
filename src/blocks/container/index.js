3a4242617e2f9bcde963d2e283218e9d439f79b8
Arya1Putra <putra1arya@gmail.com>
2026-04-01 14:23:08 +0800

    companion deprecated

--- src/blocks/container/index.js
+++ src/blocks/container/index.js
@@ -3,6 +3,7 @@ import save from './save';
 import metadata from './block.json';
 import { IconContainerSVG } from 'gutenverse-core/icons';
 import saveV1 from './deprecated/v1/save';
+import saveV2 from './deprecated/v2/save';
 
 const { name, attributes, supports } = metadata;
 
@@ -13,6 +14,11 @@ export const settings = {
     edit,
     save,
     deprecated: [
+        {
+            attributes,
+            supports,
+            save: saveV2
+        },
         {
             attributes,
             supports,
