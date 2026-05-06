3edc2babd4e7a8ab7f4c34ceac54c6be51bacae4
Arie Cristian <ari@jegstudio.com>
2026-05-06 16:15:32 +0800

    adding multiple value condition on dashboard select control

--- src/backend/forms/select.js
+++ src/backend/forms/select.js
@@ -3,32 +3,55 @@ import { v4 as uuidv4 } from 'uuid';
 import Select from 'react-select';
 
 const ControlSelect = (props) => {
-    const { id, title, description, value, options, customLabel, defaultValue = '', updateValue, isRequired = false } = props;
+    const { id, title, description = '', value, options, customLabel, defaultValue = '', updateValue, isRequired = false, isMulti = false } = props;
     let uuid = uuidv4();
-    let objValue = null;
-    options.map( el => {
-        if(el.value == value){
-            objValue = el;
-        }
-    });
-    const inputChange = (value) => {
-        updateValue(id,value.value);
-    };
-
     const inputValue = value === undefined ? defaultValue : value;
+    const objValue = isMulti ? options.filter(el => {
+        if (!Array.isArray(inputValue)) {
+            return false;
+        }
 
-    return <div className="control-wrapper control-select">
-        <label className="control-title" htmlFor={`${id}-${uuid}`} style={customLabel}>{title} {isRequired && <span style={{color:'red'}}> *</span>}</label>
-        <Select
-            id={`${id}-${uuid}`}
-            value={objValue}
-            options={options}
-            onChange={inputChange}
-        />
-        {description !== '' && <span className="control-description">
-            {description}
-        </span>}
-    </div>;
+        return inputValue.some(value => value == el.value);
+    }) : options.find(el => el.value == inputValue) || null;
+
+    const inputChange = (value) => {
+        if (isMulti) {
+            updateValue(id, value ? value.map(el => el.value) : []);
+        } else {
+            updateValue(id, value.value);
+        }
+    };
+    if (isMulti) {
+        return <div className="control-wrapper control-select multiple">
+            <div className="label-wrapper">
+                <label className="control-title" htmlFor={`${id}-${uuid}`} style={customLabel}>{title} {isRequired && <span style={{ color: 'red' }}> *</span>}</label>
+                {description !== '' && <span className="control-description">
+                    {description}
+                </span>}
+            </div>
+            <Select
+                id={`${id}-${uuid}`}
+                isMulti={isMulti}
+                value={objValue}
+                options={options}
+                onChange={inputChange}
+            />
+        </div>;
+    } else {
+        return <div className="control-wrapper control-select">
+            <label className="control-title" htmlFor={`${id}-${uuid}`} style={customLabel}>{title} {isRequired && <span style={{ color: 'red' }}> *</span>}</label>
+            <Select
+                id={`${id}-${uuid}`}
+                isMulti={isMulti}
+                value={objValue}
+                options={options}
+                onChange={inputChange}
+            />
+            {description !== '' && <span className="control-description">
+                {description}
+            </span>}
+        </div>
+    }
 };
 
 export default ControlSelect;
