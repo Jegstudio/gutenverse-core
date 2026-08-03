a7698bda4ed89f6a5916cfc221ed86c14d29cb1d
Gowinda Kadek <gowinda@jegstudio.com>
2026-08-03 14:18:12 +0800

    adjust card pro Components

--- src/components/pro/card-pro.js
+++ src/components/pro/card-pro.js
@@ -1,27 +1,50 @@
 import { activeTheme, clientUrl } from 'gutenverse-core/config';
 import ButtonUpgradePro from './button-upgrade-pro';
 import { applyFilters } from '@wordpress/hooks';
+import { __ } from '@wordpress/i18n';
 
-const CardPro = () => {
+const CardPro = ( {
+    num = null,
+    mockupLibrarySrc,
+    cube3dSrc,
+    iconLottieSrc,
+    iconNavSrc,
+    numIconSrc,
+    text = null,
+} ) => {
     const {
-        upgradeProUrl
+        upgradeProUrl,
+        imgDir
     } = window['GutenverseConfig'] || window['GutenverseDashboard'] || {};
+    const defaultMockupLibrarySrc = mockupLibrarySrc || `${imgDir}/pop-up-mockup-pro.png`;
+    const defaultCube3dSrc = cube3dSrc || `${imgDir}/pop-up-3d-cube-2.png`;
+    const defaultIconLottieSrc = iconLottieSrc || `${imgDir}/pop-up-icon-element-3.png`;
+    const defaultIconNavSrc = iconNavSrc || `${imgDir}/pop-up-icon-element-2.png`;
+    const defaultNumIconSrc = numIconSrc || `${imgDir}/pop-up-icon-element.png`;
+
     const card = <>
         <div className="guten-pro-themes-wrapper">
-            <div className="guten-card-pro-wrapper guten-pro-themes-full" style={{ backgroundImage: `url(${window['GutenverseConfig'].imgDir + '/pop-up-bg-popup-banner.png'})` }}>
+            <div className="guten-card-pro-wrapper guten-pro-themes-full" style={{ backgroundImage: `url(${imgDir + '/pop-up-bg-popup-banner.png'})` }}>
                 <div className="guten-card-pro-image-wrapper">
-                    <img className="guten-card-pro-mockup-library" src={window['GutenverseConfig'].imgDir + '/pop-up-mockup-pro.png'} />
-                    <img className="guten-card-pro-3d-cube" src={window['GutenverseConfig'].imgDir + '/pop-up-3d-cube-2.png'} />
-                    <img className="guten-card-pro-icon-lottie" src={window['GutenverseConfig'].imgDir + '/pop-up-icon-element-3.png'} />
-                    <img className="guten-card-pro-icon-nav" src={window['GutenverseConfig'].imgDir + '/pop-up-icon-element-2.png'} />
+                    <img className="guten-card-pro-mockup-library" src={defaultMockupLibrarySrc} />
+                    <img className="guten-card-pro-3d-cube" src={defaultCube3dSrc} />
+                    <img className="guten-card-pro-icon-lottie" src={defaultIconLottieSrc} />
+                    <img className="guten-card-pro-icon-nav" src={defaultIconNavSrc} />
+                    {num !== null && <img className="guten-card-pro-icon-num" src={defaultNumIconSrc} />}
                 </div>
                 <div className="guten-card-pro-content-wrapper">
                     <div className="guten-card-pro-title">
-                        Unlock Extra Features with
-                        <span> Gutenverse PRO!</span>
-                        <img className="guten-card-pro-blink" src={window['GutenverseConfig'].imgDir + '/banner-graphic-blink.png'} alt="Guten Card Pro Blink" />
+                        {text ? (
+                            text
+                        ) : (
+                            <>
+                                {__('Unlock Extra Features with', 'gutenverse-form')}
+                                <span> {__('Gutenverse PRO!', 'gutenverse-form')}</span>
+                            </>
+                        )}
+                        <img className="guten-card-pro-blink" src={imgDir + '/banner-graphic-blink.png'} alt="Guten Card Pro Blink" />
                     </div>
-                    <img className="guten-card-pro-arrow" src={window['GutenverseConfig'].imgDir + '/banner-arrow-blue.png'} alt="Guten Card Pro Arrow" />
+                    <img className="guten-card-pro-arrow" src={imgDir + '/banner-arrow-blue.png'} alt="Guten Card Pro Arrow" />
                     <ButtonUpgradePro isBanner={true} location="card-pro" link={`${upgradeProUrl}?utm_source=gutenverse&utm_medium=blockeditor&utm_client_site=${clientUrl}&utm_client_theme=${activeTheme}`} />
                 </div>
             </div>
