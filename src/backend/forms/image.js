6fc82bd388dc1f6b31a0e2e9567ebcbbed6d0cdc
tresna <tresnasaputra9@gmail.com>
2026-08-28 15:21:14 +0800

    add jnews-blocks dashboard backend style

--- src/backend/forms/image.js
+++ src/backend/forms/image.js
@@ -1,8 +1,5 @@
 
-import { MediaUploadCheck, MediaUpload } from '@wordpress/block-editor';
-import { Button } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
-import { Trash } from 'react-feather';
 import { v4 as uuidv4 } from 'uuid';
 
 const ControlImage = (props) => {
@@ -12,10 +9,8 @@ const ControlImage = (props) => {
         description,
         value = {},
         customLabel,
-        defaultValue = '',
         updateValue,
         isRequired = false,
-        allowedMediaTypes = ['image']
     } = props;
 
     let uuid = uuidv4();
@@ -62,7 +57,7 @@ const ControlImage = (props) => {
         <div className="gutenverse-icon-svg trash-icon" onClick={removeImage}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 8 10" fill="none"><path d="M5.33333 2.6V2.28C5.33333 1.83196 5.33333 1.60794 5.26067 1.43681C5.19676 1.28628 5.09477 1.16389 4.96933 1.08719C4.82672 1 4.64004 1 4.26667 1H3.73333C3.35996 1 3.17328 1 3.03067 1.08719C2.90523 1.16389 2.80324 1.28628 2.73933 1.43681C2.66667 1.60794 2.66667 1.83196 2.66667 2.28V2.6M3.33333 4.8V6.8M4.66667 4.8V6.8M1 2.6H7M6.33333 2.6V7.08C6.33333 7.75206 6.33333 8.0881 6.22434 8.34479C6.12847 8.57058 5.97549 8.75416 5.78732 8.86921C5.57341 9 5.29339 9 4.73333 9H3.26667C2.70661 9 2.42659 9 2.21268 8.86921C2.02451 8.75416 1.87153 8.57058 1.77566 8.34479C1.66667 8.0881 1.66667 7.75206 1.66667 7.08V2.6" stroke="white" stroke-width="0.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>
         </div>
-        <div class="choose-image" onClick={() => frame.open()}>
+        <div className="choose-image" onClick={() => frame.open()}>
             <span>Choose Image</span>
         </div>
     </div>;
